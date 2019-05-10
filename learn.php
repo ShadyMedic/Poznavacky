5b36a791946d6699e0fb47286b1fb08cbeb61f43
eksyska <eli.korcova@gmail.com>
2019-05-10 16:06:45 +0200

    dokonceno stylovani znacek pro klavesove zkratky

--- learn.php
+++ learn.php
@@ -45,12 +45,12 @@
     			</select>
     			<br>
     			<button onclick="prev(event)" class="button">
-				<kbd>S</kbd>
 				Předchozí přírodnina
+                <kbd>[S]</kbd>
 			</button>
     			<button onclick="next(event)" class="button">
-				<kbd>W</kbd>
 				Následující přírodnina
+                <kbd>[W]</kbd>
 			</button>
     		</fieldset>
     		<fieldset>
@@ -58,18 +58,18 @@
     				<tr>
     					<td>
     						<button onclick="prevImg()" id="prevImg">
-							<kbd>A</kbd>
-							<img src="arrow.png" style="transform: rotate(180deg);" />
-						</button>
+							<img src="arrow.png" style="transform: rotate(180deg);" /><br>
+                            <kbd>[A]</kbd>
+						    </button>
     					</td>
     					<td>
     						<img id="image" class="img" src="imagePreview.png">
     					</td>
     					<td>
     						<button onclick = "nextImg()" id="nextImg">
-							<kbd>D</kbd>
-							<img src="arrow.png" />
-						</button>
+							<img src="arrow.png" /><br>
+                            <kbd>[D]</kbd>
+						    </button>
     					</td>
     				</tr>
     			</table>
