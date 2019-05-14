2ac248a87496f87ae95c4b9c04f12eef5b08df4e
Jan Štěch <honza.stech@gmail.com>
2019-05-14 11:56:20 +0200

    Upozornění bylo základně nastylováno. Musí být ještě napsán JavaScript a vyřešen styl zavíracího tlačítka.

--- index.php
+++ index.php
@@ -13,15 +13,17 @@
         <title>Ověření</title>
     </head>
     <body id="root">
-    <main id="main_kod">
-        <div id="kod"> 
-            <span id="span_kod">Zadejte ověřovací kód:</span><br>
-            <form onsubmit="validate(event)">
-	           <input type=text maxlength=8 class="text" id="kod_input"><br>
-	           <input type=submit value="Potvrdit" class="confirm button">
-            </form> 
-        </div>
-    </main>
+    <div class="container">
+		<main id="main_kod">
+			<div id="kod"> 
+				<span id="span_kod">Zadejte ověřovací kód:</span><br>
+				<form onsubmit="validate(event)">
+				   <input type=text maxlength=8 class="text" id="kod_input"><br>
+				   <input type=submit value="Potvrdit" class="confirm button">
+				</form> 
+			</div>
+		</main>
+	</div>
 	<footer id="cookiesAlert">
 		<span>Tyto stránky využívají ke své funkci soubory cookie. Používáním stránek souhlasíte s ukládáním souborů cookie na vašem zařízení.</span>
 		<div id="cookiesAlertCloser" onclick="hideCookies">×</div>
