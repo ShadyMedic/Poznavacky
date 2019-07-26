6204a5c1108b67ba524b99811a6f690f403be709
Jan Štěch <honza.stech@gmail.com>
2019-07-26 20:54:57 +0200

    changeUsername.php nyní loguje do logu.

--- changeUsername.php
+++ changeUsername.php
@@ -2,8 +2,12 @@
     session_start();
     
     include 'connect.php';
+    include 'logger.php';
 
     $newName = $_GET['new'];
+    
+    $oldName = $_SESSION['user'];
+    $oldName = $oldName['name'];
     
     //Ochrana před SQL injekcí
     $newName = mysqli_real_escape_string($connection, $newName);
@@ -18,6 +22,7 @@
     }
     if (mysqli_num_rows($result) > 0)
     {
+        filelog("Uživatel $oldName se pokusil změnit si jméno na $newName, avšak neuspěl kvůli neunikátnímu jménu.");
         echo "Jiný uživatel již toto jméno používá, nebo o změnu na něj zažádal.";
         die();
     }
@@ -32,20 +37,19 @@
     //Kontrola délky jména
     if (strlen($newName) < 4)
     {
+        filelog("Uživatel $oldName se pokusil změnit si jméno, avšak neuspěl kvůli krátkému jménu.");
         echo "Jméno musí být alespoň 4 znaky dlouhé.";
         die();
     }
     if (strlen($newName) > 15)
     {
+        filelog("Uživatel $oldName se pokusil změnit si jméno, avšak neuspěl kvůli dlouhému jménu.");
         echo "Jméno nesmí být více než 15 znaků dlouhé.";
         die();
     }
     
     //KONTROLA DAT V POŘÁDKU
-    
-    $oldName = $_SESSION['user'];
-    $oldName = $oldName['name'];
-    
+
     //Kontrola, zda již uživatel na nějakou nevyřízenou změnu nečeká
     $query = "SELECT id FROM zadostijmena WHERE puvodni='$oldName' LIMIT 1";
     $result = mysqli_query($connection, $query);
@@ -66,6 +70,7 @@
             echo "Vyskytla se chyba při práci s databází. \nPro více informací přejděte na ".$_SERVER['SERVER_NAME']."/errSql.html";
             die();
         }
+        filelog("Uživatel $oldName se změnil svou žádost o nové jméno na $newName.");
         echo "O změnu jména bylo zažádáno. \nNové jméno bude co nejdříve zkontrolováno a případně nahradí vaše stávající jméno. \n\nTato žádost o změnu přepsala vaší nevyřízenou žádost o změnu jména z minulosti.";
     }
     else
@@ -78,5 +83,6 @@
             echo "Vyskytla se chyba při práci s databází. Pro více informací přejděte na ".$_SERVER['SERVER_NAME']."/errSql.html";
             die();
         }
+        filelog("Uživatel $oldName zažádal o změnu jména na $newName.");
         echo "O změnu jména bylo zažádáno. \nNové jméno bude co nejdříve zkontrolováno a případně nahradí vaše stávající jméno.";
     }
