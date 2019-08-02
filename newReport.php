8710b800eb2ec3f1a1a18647ef1dd03e35ee3cd2
Jan Štěch <honza.stech@gmail.com>
2019-08-02 18:08:45 +0200

    Skript newReport nyní používá při logování jméno uživatele.
    Dříve se používala IP adresa.
    Také byla přidána ochrana před SQL injekcí a lepší kontrola $_GET dat.

--- newReport.php
+++ newReport.php
@@ -10,12 +10,16 @@
 		die();
 	}
 
-	$ip = $_SERVER['REMOTE_ADDR'];
+	$username = $_SESSION['user']['name'];
 
 	$url = $_GET['pic'];
 	$reason = $_GET['reason'];
 
-	if ($reason != 0 && $reason != 1 && $reason != 2 && $reason != 3 && $reason != 4)
+	//Ochrana před SQL injekcí
+	$url = mysqli_real_escape_string($connection, $url);
+	$reason = mysqli_real_escape_string($connection, $reason);
+	
+	if ($reason !== "0" && $reason != 1 && $reason != 2 && $reason != 3 && $reason != 4)
 	{
 		die("swal('Neplatný důvod!','','error');");
 	}
@@ -25,9 +29,19 @@
 
 	$query = "SELECT id FROM $table WHERE zdroj='$url'";
 	$result = mysqli_query($connection, $query);
+	if (!$result)
+	{
+	    $err = mysqli_error($connection);
+	    die("swal('Vyskytla se neočekávaná chyba. Kontaktujte prosím správce a uveďte tuto chybu ve svém hlášení:','".mysqli_real_escape_string($connection, $err)."', 'error');");
+	}
 	$result = mysqli_fetch_array($result);
 	$picId = $result['id'];
-
+	if(empty($picId))
+	{
+	    die("swal('Neplatný obrázek','','error');");
+	}
+	
+	
 	//Zjišťování, zda je již obrázek nahlášen
 	$table = $_SESSION['current'][0].'hlaseni';
 	$pName = $_SESSION['current'][1];
@@ -47,6 +61,10 @@
 	}
 
 	mysqli_query($connection, $query);
-	filelog("Uživatel nahlásil obrázek s id $picId v poznávačce $pName z adresy $ip");
+	filelog("Uživatel $username nahlásil obrázek s id $picId v poznávačce $pName z důvodu číslo $reason.");
 	if (!mysqli_error($connection)){echo "swal('Hlášení zaznamenáno','Obrázek bude co nejdříve zkontrolován. Do té doby bude nadále zobrazován. Nenahlašujte jej prosím vícekrát.','success');";}
-	else {echo mysqli_error($connection);}
+	else
+	{
+	    $err = mysqli_error($connection);
+	    echo "swal('Vyskytla se neočekávaná chyba. Kontaktujte prosím správce a uveďte tuto chybu ve svém hlášení:','".mysqli_real_escape_string($connection, $err)."URL: $url - Query: $query', 'error');";
+	}
