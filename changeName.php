5f918dd58719b6f1f4884d47994958c8b6fae3ab
Jan Štěch <honza.stech@gmail.com>
2019-08-06 21:04:29 +0200

    Přijímání žádosti o změnu jména nyní funguje.
    Bude potřeba nějak informovat uživatele, že se pod starým jménem již
    nepřihlásí.

---
+++ changeName.php
@@ -0,0 +1,33 @@
+<?php
+    include 'httpStats.php';     //Obsahuje session_start();
+    session_start();
+    
+    //Kontrola, zda je uživatel administrátorem.
+    $username = $_SESSION['user']['name'];
+    $query = "SELECT status FROM uzivatele WHERE jmeno='$username' LIMIT 1";
+    $result = mysqli_query($connection, $query);
+    if (!$result)
+    {
+        header("Location: errSql.html");
+        die();
+    }
+    $status = mysqli_fetch_array($result)['status'];
+    if ($status !== 'admin')
+    {
+        //Zamítnutí přístupu
+        die();
+    }
+    
+    $oldName = $_POST['oldName'];
+    $newName = $_POST['newName'];
+    
+    $oldName = mysqli_real_escape_string($connection, $oldName);
+    $newName = mysqli_real_escape_string($connection, $newName);
+    
+    //Změna jména
+    $query = "UPDATE uzivatele SET jmeno = '$newName' WHERE jmeno = '$oldName'";
+    $result = mysqli_query($connection, $query);
+    
+    //Odstraňování žádosti
+    $query = "DELETE FROM zadostijmena WHERE puvodni='$oldName'";
+    $result = mysqli_query($connection, $query);
--- management.js
+++ management.js
@@ -46,8 +46,17 @@ function fifthTab()
 /*------------------------------------------------------------*/
 function acceptNameChange(event)
 {
-	//TODO implementovat funkci pro přijmutí změny jména
-	console.log("Accepted.");
+	//Získání současného jména
+	var oldName = event.target.parentNode.parentNode.parentNode.childNodes[0].innerHTML;
+	
+	//Získání nového jména
+	var newName = event.target.parentNode.parentNode.parentNode.childNodes[1].innerHTML;
+	
+	//Posílání požadavku na ovlivnění databáze
+	postRequest("changeName.php", null, null, oldName, newName);
+	
+	//Odstranění požadavku z DOM
+	event.target.parentNode.parentNode.parentNode.parentNode.removeChild(event.target.parentNode.parentNode.parentNode);
 }
 function declineNameChange(event)
 {
@@ -58,4 +67,52 @@ function sendMailNameChange(event)
 {
 	//TODO implementovat funkci pro odeslání e-mailu žadateli o změnu jména
 	console.log("Sent.");
+}
+/*------------------------------------------------------------*/
+function postRequest(url, success = null, error = null, oldName = null, newName = null)
+{
+	var req = false;
+	//Creating request
+	try
+	{
+		//Most broswers
+		req = new XMLHttpRequest();
+	} catch (e)
+	{
+		//Interned Explorer
+		try
+		{
+			req = new ActiveXObject("Msxml2.XMLHTTP");
+		}catch(e)
+		{
+			//Older version of IE
+			try
+			{
+				req = new ActiveXObject("Microsoft.XMLHTTP");
+			}catch(e)
+			{
+				return false;
+			}
+		}
+	}
+	
+	//Checking request
+	if (!req) return false;
+	
+	//Checking function parameters and setting intial values in case they aren´t specified
+	if (typeof success != 'function') success = function () {};
+	if (typeof error!= 'function') error = function () {};
+	
+	//Waiting for server response
+	req.onreadystatechange = function()
+	{
+		if(req.readyState == 4)
+		{
+			return req.status === 200 ? success(req.responseText) : error(req.status);
+		}
+	}
+	req.open("POST", url, true);
+	req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	req.send("oldName="+oldName+"&newName="+newName);
+	return req;
 }
