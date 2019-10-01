29f11725d7497c454f99d7813cd33e513dfd18cb
Jan Štěch <honza.stech@gmail.com>
2019-10-01 07:42:24 +0200

    Aktualizována reference na stylesheet

--- emailPasswordRecovery.php
+++ emailPasswordRecovery.php
@@ -7,7 +7,7 @@
 	<head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width" />
-		<link rel="stylesheet" type="text/css" href="css.css">
+		<link rel="stylesheet" type="text/css" href="css/css.css">
 		<script type="text/javascript" src="emailPasswordRecovery.js"></script>
 		<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 		<title>Obnova hesla</title>
