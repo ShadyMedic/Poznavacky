b1b1743baef94c9c41f125b2efbc048bf68c51e1
Jan Štěch <honza.stech@gmail.com>
2019-10-05 16:55:28 +0200

    Aktualizován odkaz na newReport.php

--- jScript/learn.js
+++ jScript/learn.js
@@ -142,7 +142,7 @@ function submitReport(event)
 		return;
 	}
 	
-	getRequest("newReport.php?pic=" + picUrl + "&reason=" + reason, reportResponse);
+	getRequest("php/ajax/newReport.php?pic=" + picUrl + "&reason=" + reason, reportResponse);
 }
 function cancelReport(event)
 {
