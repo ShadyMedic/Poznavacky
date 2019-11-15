d5d54d107f804d89ddf2028f1510e0ea4cda7548
Jan Štěch <honza.stech@gmail.com>
2019-11-15 17:57:27 +0100

    getPics.php nyní pracuje s novou strukturou databáze.
    SQL dotazy navíc byly optimalizovány.

--- php/ajax/getPics.php
+++ php/ajax/getPics.php
@@ -20,10 +20,9 @@
 	$name = mysqli_real_escape_string($connection, $name);
 	
 	//Zjišťování počtu obrázků
-	$table = $_SESSION['current'][0].'seznam';
 	$pName = $_SESSION['current'][1];
 
-	$query = "SELECT id,obrazky FROM $table WHERE nazev='$name'";
+	$query = "SELECT id,obrazky FROM prirodniny WHERE nazev='$name' LIMIT 1";
 	$result = mysqli_query($connection, $query);
 	if ($result && mysqli_num_rows($result) > 0){$result = mysqli_fetch_array($result);}
 	else{die("swal('Neplatný název!','','error');");}
@@ -35,15 +34,10 @@
 	if($amount > 0){$number %= $amount;}
 
 	//Získávání URL obrázku
-	$table = $_SESSION['current'][0].'obrazky';
-
-	$query = "SELECT zdroj FROM $table WHERE prirodninaId=$id AND povoleno=1";
+	$query = "SELECT * FROM (SELECT id,zdroj FROM obrazky WHERE prirodninaId=$id AND povoleno=1 LIMIT ".($number+1).") AS zdroje ORDER BY id DESC LIMIT 1";
 	$result = mysqli_query($connection, $query);
 	if (gettype($result) !== "object" || mysqli_num_rows($result) <= 0){die("images/noImage.png");}
-	for($i = 0; $i <= $number; $i++)
-	{
-		$resultArr = mysqli_fetch_array($result);
-	}
+	$resultArr = mysqli_fetch_array($result);
 	$resultArr = $resultArr['zdroj'];
 	$username = $_SESSION['user']['name'];
 	filelog("K uživateli $username byl odeslán obrázek pro učící stránku pro poznávačku $pName.");
