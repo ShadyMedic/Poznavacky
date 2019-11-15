05bfb4717e3e808cd43a06bee45633774f8793a9
Jan Štěch <honza.stech@gmail.com>
2019-11-15 20:47:08 +0100

    getRandomPic.php nyní pracuje s novou strukturou databáze.
    Náhodný výběr obrázku byl optimalizován.

--- php/ajax/getRandomPic.php
+++ php/ajax/getRandomPic.php
@@ -10,33 +10,33 @@
 		die();
 	}
 	
-	$username = $_SESSION['user']['name'];
+	$pId = $_SESSION['current'][0];
+	$pId = mysqli_real_escape_string($connection, $pId);
 	
-	
-	$table = $_SESSION['current'][0].'seznam';
-	
-	//Získávání náhodné přírodniny
-	/*
-	 * Poznámka: tento způsob náhodného výběru je neefektivní pro velké tabulky,
-	 * ale pro seznamové tabulky je naprosto v pořádku, protože obsahují maximálně 50 - 100 záznamů.
-	 */
-	$query = "SELECT id,nazev,obrazky FROM $table WHERE obrazky > 0 ORDER BY RAND() LIMIT 1";
+	//Získání náhodného čísla v rozmezí 0 až počet přírodnin ve zvolené poznávačce
+	$query = "SELECT CEIL(RAND() *(SELECT COUNT(*) FROM prirodniny WHERE cast = $pId))AS randNum";
 	$result = mysqli_query($connection, $query);
+	if (!$result){echo mysqli_error($connection);}
 	$result = mysqli_fetch_array($result);
-	$answer = $result['nazev'];
-	$id = $result['id'];
+	$rand = $result['randNum'];
 	
-	$table = $_SESSION['current'][0].'obrazky';
-	
-	//Získávání seznamu obrázků dané přírodniny
-	$query = "SELECT zdroj FROM $table WHERE prirodninaId = $id AND povoleno = 1";
+	//Získání ID a názvu náhodné přírodniny patřící do zvolené poznávačky
+	$query = "SELECT id,nazev FROM prirodniny WHERE cast = $pId ORDER BY id ASC LIMIT 1 OFFSET $rand";
 	$result = mysqli_query($connection, $query);
-	$randIndex = rand(0,mysqli_num_rows($result) - 1);
-	mysqli_data_seek($result, $randIndex);
-	$row = mysqli_fetch_array($result);
+	if (!$result){echo mysqli_error($connection);}
+	$result = mysqli_fetch_array($result);
+	$id = $result['id'];
+	$answer = $result['nazev'];
+	
+	//Získávání náhodného obrázků dané přírodniny
+	$query = "SELECT zdroj FROM obrazky WHERE prirodninaId = $id AND povoleno = 1 ORDER BY RAND() LIMIT 1";
+	$result = mysqli_query($connection, $query);
+	if (!$result){echo mysqli_error($connection);}
+	$result = mysqli_fetch_array($result);
+	$source = $result['zdroj'];
 	
 	//Odesílání dat
-	echo $row['zdroj'];
+	echo $source;
 	echo "¶";
 	echo $answer;
 	
@@ -44,5 +44,6 @@
 	$_SESSION['testAnswer'] = $answer;
     
 	//Logování
+	$username = $_SESSION['user']['name'];
 	$pName = $_SESSION['current'][1];
 	filelog("K uživateli $username byl odeslán obrázek pro zkoušecí stránku pro poznávačku $pName.");
