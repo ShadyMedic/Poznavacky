94b1e510cb3f389ca409af1eb7cad513d56bd921
Jan Štěch <honza.stech@gmail.com>
2019-12-28 14:48:35 +0100

    Zprovozněno themeHandler.php
    themeHandler nyní načítá hodnoty proměnných z nového souboru themes.xml.
    Hodnoty proměnných v themes.xml nejsou finální, pouze zástupné.
    Do list_of_files.txt byly přidány informace o themes.xml a aktualizovány informace o CONSTANTS.php.

--- php/included/themeHandler.php
+++ php/included/themeHandler.php
@@ -1,2 +1,30 @@
 <?php
-  //TODO
+   function createCssVariable($name, $value)
+    {
+        echo "--$name: $value; ";
+    }
+    
+    echo ":root{";
+    
+    $themeId = @$_SESSION['user']['theme'];    //Session nastartováno ve verification.php
+    $themeId = (int)$themeId;
+    
+    $xmlData = simplexml_load_file('themes.xml') or die('</style><script>alert("Chyba při načítání vzhledu.\nVypadá to, že se někde stala chyba při načítání vámi nastaveného vzhledu stránek.\nZkuste to prosím později a pokud tato chyba přetrvá, kontaktujte prosím podporu.");</script></head><body></body></html>');
+    try
+    {
+        if (!in_array($themeId, [0,1,2,3,4,5], true)){throw new Exception();}
+        createCssVariable('barva1', $xmlData->theme[$themeId]->color[0]);
+        createCssVariable('barva2', $xmlData->theme[$themeId]->color[1]);
+        createCssVariable('barva3', $xmlData->theme[$themeId]->color[2]);
+        createCssVariable('bgPic', $xmlData->theme[$themeId]->picture);
+    }
+    catch(Exception $e)
+    {
+        echo '}</style><script>alert("Chyba při načítání vzhledu.\nVypadá to, že se někde stala chyba při načítání vámi nastaveného vzhledu stránek.\nZkuste se prosím odhlásit a přihlásit a pokud tato chyba přetrvá, kontaktujte prosím podporu.\nNyní se načte základní vzhled.");</script><style>:root{';
+        require 'php/included/CONSTANTS.php';
+        createCssVariable('barva1', $xmlData->theme[DEFAULT_THEME]->color[0]);
+        createCssVariable('barva2', $xmlData->theme[DEFAULT_THEME]->color[1]);
+        createCssVariable('barva3', $xmlData->theme[DEFAULT_THEME]->color[2]);
+        createCssVariable('bgPic', $xmlData->theme[DEFAULT_THEME]->picture);
+    }
+    echo "}";
