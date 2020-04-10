c64c9895ace01f0710f884af4ecf0aad05d79d33
Jan Štěch <honza.stech@gmail.com>
2020-04-10 13:46:34 +0200

    Zvrácení commitu 343ac686c103e466f38514b9e621a8acaa181fa1
    Commit zajišťoval kontrolu, zda uživatel má přístup do třídy na stránkách addPics.php, learn.php a test.php.

--- php/included/partSetter.php
+++ php/included/partSetter.php
@@ -1,7 +1,6 @@
 <?php
     //Nastavování současné části
     $pId = @$_COOKIE['current'];
-    $classId = $_SESSION['class'];
     
     $everything = false;
     
@@ -21,39 +20,19 @@
             $result = mysqli_fetch_array($result);
             $pName = $result['nazev'].' - Vše';
             $pId = $result['poznavacky_id'];
-            
-            //Kontrola, zda zvolená poznávačka patří do dříve zvolené třídy
-            //Tím je znemožněn výběr poznávačky patřící do třídy, v níž uživatel není členem
-            $query = "SELECT COUNT(*) AS 'cnt' FROM poznavacky WHERE poznavacky_id = $pId AND tridy_id = $classId";
-            if (mysqli_fetch_array(mysqli_query($connection, $query))['cnt'] < 1)
-            {
-                //Zamítnutí přístupu
-                header("Location: err403.html");
-                die();
-            }
         }
     }
     else
     {
         //Zjištění jmena části
-        include 'php/included/connect.php';
         $pId = mysqli_real_escape_string($connection, $pId);
+        include 'php/included/connect.php';
         if (!empty($pId))
         {
             $query = "SELECT nazev FROM casti WHERE casti_id=$pId LIMIT 1";
             $result = mysqli_query($connection, $query);
             $pName = mysqli_fetch_array($result);
             $pName = $pName['nazev'];
-            
-            //Kontrola, zda zvolená část patří do poznávačky patřící do dříve zvolené třídy
-            //Tím je znemožněn výběr části patřící poznávačky, která je součástí třídy, v níž uživatel není členem
-            $query = "SELECT COUNT(*) AS 'cnt' FROM casti WHERE casti_id = $pId AND poznavacky_id IN (SELECT poznavacky_id FROM poznavacky WHERE tridy_id = $classId)";
-            if (mysqli_fetch_array(mysqli_query($connection, $query))['cnt'] < 1)
-            {
-                //Zamítnutí přístupu
-                header("Location: err403.html");
-                die();
-            }
         }
     }
     //Mazání cookie current
