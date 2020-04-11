c6015a00e0ba899f4af8abc4f890cb1b5e9a3cb2
eksyska <eli.korcova@gmail.com>
2020-04-11 20:06:24 +0200

    nastylováno zadávání admin hesla
    bylo nutné přesunut php část do html, aby byly aplikovány styly
    bohužel, kvůli způsobu vkládání elementů, nebylo možné je nastylovat úplně ideálně, ale myslím, že to snad stačí i takhle

--- management.php
+++ management.php
@@ -1,64 +1,3 @@
-<?php    
-    $redirectIn = false;
-    $redirectOut = true;
-    require 'php/included/verification.php';    //Obsahuje session_start();
-    
-    //Kontrola, zda je uživatel administrátorem.
-    $username = $_SESSION['user']['name'];
-    $query = "SELECT status FROM uzivatele WHERE jmeno='$username' LIMIT 1";
-    $result = mysqli_query($connection, $query);
-    if (!$result)
-    {
-        header("Location: errSql.html");
-        die();
-    }
-    $status = mysqli_fetch_array($result)['status'];
-    if ($status !== 'admin')
-    {
-        //Zamítnutí přístupu
-        header("Location: err403.html");
-        die();
-    }
-    
-    //Heslo raději znovu načtu z databáze - nebudu používat hash uložený v $_SESSION['user']['hash']
-    $query = "SELECT heslo FROM uzivatele WHERE jmeno='$username' LIMIT 1";
-    $result = mysqli_query($connection, $query);
-    if (!$result)
-    {
-        header("Location: errSql.html");
-        die();
-    }
-    $adminHash = mysqli_fetch_array($result)['heslo'];
-    //Kontrola zadaného hesla
-    if (isset($_POST['adminPassword']) && !password_verify(@$_POST['adminPassword'], $adminHash))
-    {
-        echo "<span>Špatné heslo</span>";
-    }
-    if (!isset($_POST['adminPassword']) || !password_verify(@$_POST['adminPassword'], $adminHash))
-    {
-        echo "
-            <form action='management.php' method=POST>
-                <input type=password maxlength=31 placeholder='Zadejte administrátorské heslo' name='adminPassword'>
-                <input type=submit value='OK'>
-            </form>
-        ";
-    }
-    
-    if (password_verify(@$_POST['adminPassword'], $adminHash))
-    {
-        //Vymaž zadané heslo a počkej na vykreslení stránky
-        unset($_POST['adminPassword']);
-    }
-    else
-    {
-        //Vymaž zadané heslo
-        unset($_POST['adminPassword']);
-        
-        //Nevykresluj stránku - admin není autorizován
-        die();
-    }
-?>
-
 <!DOCTYPE html>
 <html>
     <head>
@@ -68,6 +7,66 @@
     <title>Správa služby</title>
 	</head>
     <body>
+		<?php    
+			$redirectIn = false;
+			$redirectOut = true;
+			require 'php/included/verification.php';    //Obsahuje session_start();
+			
+			//Kontrola, zda je uživatel administrátorem.
+			$username = $_SESSION['user']['name'];
+			$query = "SELECT status FROM uzivatele WHERE jmeno='$username' LIMIT 1";
+			$result = mysqli_query($connection, $query);
+			if (!$result)
+			{
+				header("Location: errSql.html");
+				die();
+			}
+			$status = mysqli_fetch_array($result)['status'];
+			if ($status !== 'admin')
+			{
+				//Zamítnutí přístupu
+				header("Location: err403.html");
+				die();
+			}
+			
+			//Heslo raději znovu načtu z databáze - nebudu používat hash uložený v $_SESSION['user']['hash']
+			$query = "SELECT heslo FROM uzivatele WHERE jmeno='$username' LIMIT 1";
+			$result = mysqli_query($connection, $query);
+			if (!$result)
+			{
+				header("Location: errSql.html");
+				die();
+			}
+			$adminHash = mysqli_fetch_array($result)['heslo'];
+			//Kontrola zadaného hesla
+			if (isset($_POST['adminPassword']) && !password_verify(@$_POST['adminPassword'], $adminHash))
+			{
+				echo "<span id='wrongAdminPassword'>Zadali jste špatné heslo nebo nemáte pozici admina</span>";
+			}
+			if (!isset($_POST['adminPassword']) || !password_verify(@$_POST['adminPassword'], $adminHash))
+			{
+				echo "
+					<form id='adminCheck' action='management.php' method=POST>
+						<input type=password maxlength=31 placeholder='Zadejte heslo' name='adminPassword'>
+						<input type=submit value='OK'>
+					</form>
+				";
+			}
+			
+			if (password_verify(@$_POST['adminPassword'], $adminHash))
+			{
+				//Vymaž zadané heslo a počkej na vykreslení stránky
+				unset($_POST['adminPassword']);
+			}
+			else
+			{
+				//Vymaž zadané heslo
+				unset($_POST['adminPassword']);
+				
+				//Nevykresluj stránku - admin není autorizován
+				die();
+			}
+		?>
 		<div class="overlay"></div>
 	<div class="container">
 		<header>
