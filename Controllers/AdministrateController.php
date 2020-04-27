5ddd4be00b4f9d32846f0e5f4a77b22caef48956
Jan Štěch <honza.stech@gmail.com>
2020-04-27 22:18:43 +0200

    Přidání kontroleru a pohledu pro stránku se správou služby.
    Dále byla přidána metoda do třídy AccessChecker, která kontroluje, zda je přihlášený uživatel administrátorem.
    Také byl zkopírován příslušný CSS soubor pro administrátorskou stránku zpět do kořenové složky.

---
+++ Controllers/AdministrateController.php
@@ -0,0 +1,30 @@
+<?php
+
+/** 
+ * Kontroler starající se o výpis administrační stránky správcům služby
+ * @author Jan Štěch
+ */
+class AdministrateController extends Controller
+{
+
+    /**
+     * Metoda ověřující, zda má uživatel do administrace přístup a nastavující hlavičku stránky a pohled
+     * @see Controller::process()
+     */
+    public function process(array $paremeters)
+    {
+        if (!AccessChecker::checkSystemAdmin(UserManager::getId()))
+        {
+            $this->redirect('error403');
+        }
+        
+        $this->pageHeader['title'] = 'Správa služby';
+        $this->pageHeader['description'] = 'Nástroj pro administrátory služby umožňující snadnou správu různých součástí systému.';
+        $this->pageHeader['keywords'] = '';
+        $this->pageHeader['cssFile'] = 'css/private.css';
+        $this->pageHeader['jsFile'] = 'js/private.js';
+        $this->pageHeader['bodyId'] = 'administrate';
+        
+        $this->view = 'administrate';
+    }
+}
--- Models/AccessChecker.php
+++ Models/AccessChecker.php
@@ -19,10 +19,27 @@ class AccessChecker
         return ($result['cnt'] === 1) ? true : false;
     }
     
+    /**
+     * Metoda kontrolující, zda je určitý uživatel správcem určité třídy
+     * @param int $userId ID ověřovaného uživatele
+     * @param int $classId ID ověřované třídy
+     * @return boolean TRUE, pokud je uživatelem správce třídy, FALSE pokud ne
+     */
     public static function checkAdmin(int $userId, int $classId)
     {
         Db::connect();
         $result = Db::fetchQuery('SELECT COUNT(*) AS "cnt" FROM `tridy` WHERE tridy_id = ? AND spravce = ?;', array($classId, $userId), false);
+        return ($result['cnt'] === 1) ? true : false;
+    }
+    
+    /**
+     * Metoda kontrolující, zda je určitý uživatel systémovým správcem
+     * @param int $userId ID ověřovaného uživatele
+     */
+    public static function checkSystemAdmin(int $userId)
+    {
+        Db::connect();
+        $result = Db::fetchQuery('SELECT COUNT(*) AS "cnt" FROM uzivatele WHERE uzivatel_id = ? AND status = "admin"', array($userId), false);
         return ($result['cnt'] === 1) ? true : false;
     }
 }
