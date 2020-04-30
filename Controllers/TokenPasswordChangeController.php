c7db440ff40c9a177954c69298a765620efc2f94
Jan Štěch <honza.stech@gmail.com>
2020-04-30 19:52:36 +0200

    Přidání modelů a kontroleru pro zpracování informací pro obnovu hesel
    Viz phpDoc pro více informací.

---
+++ Controllers/TokenPasswordChangeController.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Kontroler starající se o zpracování dat odeslaných z formuláře pro obnovení hesla
+ * @author Jan Štěch
+ */
+class TokenPasswordChangeController extends Controller
+{
+
+    /**
+     * Kontroler přijímající data odeslaná formulářem a volající modely, které je zpracují.
+     * Také nastavuje úspěchovou nebo chybové hlášky a přesměrovává zpět nebo nastavuje pohled pro zobrazení zprávy.
+     * @see Controller::process()
+     */
+    public function process(array $parameters)
+    {
+        $token = $_POST['token'];
+        $pass = $_POST['pass'];
+        $repass = $_POST['repass'];
+        
+        try
+        {
+            $passwordChanger = new TokenPasswordChanger($token, $pass, $repass);
+            $passwordChanger->verifyToken();
+            $passwordChanger->checkPasswords();
+            $passwordChanger->changePassword();
+            $passwordChanger->devalueToken();
+        }
+        catch (AccessDeniedException $e)
+        {
+            setcookie('recoveryErrorMessage', $e->getMessage());
+            $this->redirect('recoverPassword/'.$token);
+        }
+        
+        $this->data['message'] = 'Heslo bylo úspěšně změněno';
+        $this->pageHeader['bodyId'] = 'recoverPasswordMessage';
+        $this->view = 'recoverPasswordMessage';
+    }
+}
+
---
+++ Models/TokenPasswordChanger.php
@@ -0,0 +1,102 @@
+<?php
+/** 
+ * Třída umožňující změnu hesla uživatele po odeslání formuláře na recover-password stránce
+ * @author Jan Štěch
+ */
+class TokenPasswordChanger
+{
+    private $token;
+    private $pass;
+    private $repass;
+    private $userId;
+    private $verified = false;
+    private $checked = false;
+    
+    /**
+     * Konstruktor nastavující privátní vlastnosti objektu které jsou později ověřovány
+     * @param string $token Kód pro obnovu hesla odeslaný z formuláře na password-recovery stránce
+     * @param string $pass Heslo odeslané z formuláře
+     * @param string $repass Heslo znovu odeslané z formuláře
+     */
+    public function __construct(string $token, string $pass, string $repass)
+    {
+        $this->token = $token;
+        $this->pass = $pass;
+        $this->repass = $repass;
+    }
+    
+    /**
+     * Metoda ověřující platnost kódu pro obnovení hesla a ukládající ID uživatele s jehož účtem je svázán
+     * @throws AccessDeniedException Pokud není kód v databázi nalezen
+     */
+    public function verifyToken()
+    {
+        PasswordRecoveryCodeVerificator::deleteOutdatedCodes();
+        $this->userId = PasswordRecoveryCodeVerificator::verifyCode($this->token);
+        if (empty($this->userId))
+        {
+            throw new AccessDeniedException(AccessDeniedException::REASON_RECOVER_INVALID_TOKEN);
+        }
+        $this->verified = true;
+    }
+    
+    /**
+     * Metoda odstraňující použitý kód pro obnovu hesla z databáze
+     */
+    public function devalueToken()
+    {
+        PasswordRecoveryCodeVerificator::deleteCode($this->token);
+    }
+    
+    /**
+     * Metoda ověřující, zda je možné zadané heslo použít
+     * @throws AccessDeniedException Pokud se hesla neshodují
+     */
+    function checkPasswords()
+    {
+        $validator = new DataValidator();
+        try
+        {
+            $validator->checkLength($this->pass, 6, 31, 1);
+            $validator->checkCharacters($this->pass, '0123456789aábcčdďeěéfghiíjklmnňoópqrřsštťuůúvwxyýzžAÁBCČDĎEĚÉFGHIÍJKLMNŇOÓPQRŘSŠTŤUŮÚVWXYZŽ {}()[]#:;^,.?!|_`~@$%/+-*=\"\'', 1);
+        }
+        catch (RangeException $e)
+        {
+            if ($e->getMessage() === 'long')
+            {
+                throw new AccessDeniedException(AccessDeniedException::REASON_REGISTER_PASSWORD_TOO_LONG, null, null, array('originalFile' => 'TokenPasswordChanger.php', 'displayOnView' => 'recoverPassword.phtml'));
+            }
+            else if ($e->getMessage() === 'short')
+            {
+                throw new AccessDeniedException(AccessDeniedException::REASON_REGISTER_PASSWORD_TOO_SHORT, null, null, array('originalFile' => 'TokenPasswordChanger.php', 'displayOnView' => 'recoverPassword.phtml'));
+            }
+        }
+        catch (InvalidArgumentException $e)
+        {
+            throw new AccessDeniedException(AccessDeniedException::REASON_REGISTER_PASSWORD_INVALID_CHARACTERS, null, null, array('originalFile' => 'TokenPasswordChanger.php', 'displayOnView' => 'recoverPassword.phtml'));
+        }
+        
+        if ($this->pass !== $this->repass)
+        {
+            throw new AccessDeniedException(AccessDeniedException::REASON_REGISTER_DIFFERENT_PASSWORDS, null, null, array('originalFile' => 'TokenPasswordChanger.php', 'displayOnView' => 'recoverPassword.phtml'));
+        }
+        $this->checked = true;
+    }
+    
+    /**
+     * Metoda měnící heslo uživatele
+     * @throws RuntimeException Pokud zatím nebyl ověřen kód pro obnovu hesla nebo platnost hesel
+     * @return boolean TRUE, pokud je heslo úspěšně změněno
+     */
+    function changePassword()
+    {
+        if (!($this->verified && $this->checked))
+        {
+            throw new RuntimeException('Zatím nebyl ověřen kód pro obnovu hesla nebo platnost hesel. Pokud toto čtete, kontaktujte prosím správce');
+        }
+        Db::connect();
+        Db::executeQuery('UPDATE uzivatele SET heslo = ? WHERE uzivatele_id = ?', array(password_hash($this->pass, PASSWORD_DEFAULT), $this->userId));
+        return true;
+    }
+}
+
