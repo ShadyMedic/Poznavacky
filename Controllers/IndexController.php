0015ef4a53631b5ba115a31ef54776c4ddf733b6
eksyska <eli.korcova@gmail.com>
2020-05-02 10:17:37 +0200

    úprava keywords a description

--- Controllers/IndexController.php
+++ Controllers/IndexController.php
@@ -13,8 +13,8 @@ class IndexController extends Controller
     public function process(array $paremeters)
     {
         $this->pageHeader['title'] = 'Poznávačky';
-        $this->pageHeader['description'] = 'Čeká vás poznávačka z biologie? Není lepší způsob jak se na ní naučit než použitím této webové aplikace. Přidejte si vlastní poznávačku, společně do ní přidávejte obrázky, učte se z nich a nechte si generovat náhodné testy. To vše kompletně zdarma.';
-        $this->pageHeader['keywords'] = 'poznávačky, biologie, příroda';
+        $this->pageHeader['description'] = 'Čeká vás poznávačka z biologie? Není lepší způsob, jak se na ni naučit, než použitím této webové aplikace. Vytvořte si vlastní poznávačku, společně do ní přidávejte obrázky, učte se z nich a nechte si generovat náhodné testy.';
+        $this->pageHeader['keywords'] = 'poznávačky, biologie, příroda, poznávačka, přírodopis, přírodověda, test, výuka, naučit, učit, testy, učení';
         $this->pageHeader['cssFile'] = 'css/css.css';
         $this->pageHeader['jsFile'] = 'js/index.js';
         $this->pageHeader['bodyId'] = 'index';
