0778d709bd467fa6a5ad4ad5ac2cdae6b4d343e2
Jan Štěch <honza.stech@gmail.com>
2020-05-04 23:30:40 +0200

    Odebrání nepotřebné atributy $tableLevel z NoDataException

--- Models/NoDataException.php
+++ Models/NoDataException.php
@@ -9,28 +9,15 @@ class NoDataException extends Exception
     const NO_GROUPS = 'V této třídě nejsou žádné poznávačky';
     const NO_PARTS = 'V této poznávačce nejsou žádné části';
     
-    private $tableLevel;
-    
     /**
      * Konstruktor podmínky
      * @param string $message Obecná zpráva, která může být zobrazena běžnému uživateli
      * @param int $code Číslo chyby, které může být zobrazeno běžnému uživateli
      * @param Exception $previous Předcházející podmínka (pro účely propagace podmínek)
-     * @param int $tableLevel Úroveň prohlížené složky v menu tabulce - 0 = tabulka tříd; 1 = tabulka poznávaček; 2 = tabulka částí
      */
-    public function __construct(string $message = null, int $code = null, Exception $previous = null, int $tableLevel)
+    public function __construct(string $message = null, int $code = null, Exception $previous = null)
     {
         parent::__construct($message, $code, $previous);
-        $this->tableLevel = $tableLevel;
-    }
-    
-    /**
-     * Funkce navracející úroveň prohlížené složky v menu tabulce
-     * @return int Celé číslo - 0 = tabulka tříd; 1 = tabulka poznávaček; 2 = tabulka částí
-     */
-    public function getTableLevel()
-    {
-        return $this->tableLevel;
     }
 }
 
