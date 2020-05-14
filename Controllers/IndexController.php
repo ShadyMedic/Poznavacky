1ef6a1ea14d1249cb5224ecf0aea34fe87cc0a5a
Jan Štěch <honza.stech@gmail.com>
2020-05-14 12:02:25 +0200

    Na index stránce se již nyní neobnovují hodnoty <input> polí po odeslání
    Po odeslání formulářů se totiž v případě neúspěchu stránka neaktualizuje a data uložená v polích pro vstup se tak neztratí.
    Toho je docíleno tím, že požadavky se odesílají AJAXově.

--- Controllers/IndexController.php
+++ Controllers/IndexController.php
@@ -25,8 +25,7 @@ class IndexController extends Controller
             catch(AccessDeniedException $e)
             {
                 //Kód nebyl platný
-                $_SESSION['error']['form'] = $e->getAdditionalInfo('form');
-                $_SESSION['error']['message'] = $e->getMessage();
+                $this->addMessage(MessageBox::MESSAGE_TYPE_ERROR, $e->getMessage());
                 
                 //Vymaž cookie s neplatným kódem
                 setcookie('instantLogin', null, -1);
@@ -40,50 +39,6 @@ class IndexController extends Controller
         $this->pageHeader['cssFile'] = 'css/css.css';
         $this->pageHeader['jsFile'] = 'js/index.js';
         $this->pageHeader['bodyId'] = 'index';
-        
-        //Práce s chybovými hláškami
-        $this->data['loginError'] = '';
-        $this->data['registerError'] = '';
-        $this->data['passRecoveryError'] = '';
-        $this->data['passRecoverySuccess'] = '';
-        
-        //Zkontrolovat, zda nejsou nějaké chybové nebo úspěchové hlášky k zobrazení
-        if (isset($_SESSION['error']))
-        {
-            $index = $_SESSION['error']['form'].'Error';
-            $this->data[$index] = $_SESSION['error']['message'];
-            unset($_SESSION['error']);
-        }
-        if (isset($_SESSION['success']))
-        {
-            $index = $_SESSION['success']['form'].'Success';
-            $this->data[$index] = $_SESSION['success']['message'];
-            unset($_SESSION['success']);
-        }
-        
-        //Práce s dříve vyplněnými formuláři
-        $this->data['loginName'] = '';
-        $this->data['loginPass'] = '';
-        $this->data['registerName'] = '';
-        $this->data['registerPass'] = '';
-        $this->data['registerRepass'] = '';
-        $this->data['registerEmail'] = '';
-        $this->data['passRecoveryEmail'] = '';
-        
-        if (isset($_SESSION['previousAnswers']))
-        {
-            //Atributa name ve formulářích na index stránce je u každého pole nastavena na stejný název jako proměnná ze které se vypisuje hodnota atributy value
-            $previousAnswers = unserialize($_SESSION['previousAnswers']);
-            $this->data['loginName'] = @$previousAnswers['loginName'];
-            $this->data['loginPass'] = @$previousAnswers['loginPass'];
-            $this->data['registerName'] = @$previousAnswers['registerName'];
-            $this->data['registerPass'] = @$previousAnswers['registerPass'];
-            $this->data['registerRepass'] = @$previousAnswers['registerRepass'];
-            $this->data['registerEmail'] = @$previousAnswers['registerEmail'];
-            $this->data['passRecoveryEmail'] = @$previousAnswers['passRecoveryEmail'];
-            
-            unset($_SESSION['previousAnswers']);
-        }
         
         $this->view = 'index';
     }
