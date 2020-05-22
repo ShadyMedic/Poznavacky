88d9275b5c1282f7669f91b4234df5584ae2f7a3
Jan Štěch <honza.stech@gmail.com>
2020-05-22 21:20:08 +0200

    Kontroler pro addPictures stránku nyní dodává všechna potřebná data
    Funguje velice podobně jako kontroler pro learn stránku

--- Controllers/AddPicturesController.php
+++ Controllers/AddPicturesController.php
@@ -18,6 +18,23 @@ class AddPicturesController extends Controller
             $this->redirect('error403');
         }
         
+        $class = new ClassObject(0, $parameters[0]);
+        $group = new Group(0, $parameters[1], $class);
+        if (isset($parameters[2]))
+        {
+            $part = new Part(0, $parameters[2], $group);
+            $allParts = false;
+        }
+        else
+        {
+            $allParts = true;
+        }
+        
+        if ($allParts){ $this->data['naturals'] = $group->getNaturals(); }
+        else { $this->data['naturals'] = $part->getNaturals(); }
+        
+        $this->data['returnUrl'] = 'menu/'.$class->getName().'/'.$group->getName();
+        
         $this->pageHeader['title'] = 'Přidat obrázky';
         $this->pageHeader['description'] = 'Přidávejte obrázky do své poznávačky, aby se z nich mohli učit všichni členové třídy';
         $this->pageHeader['keywords'] = '';
