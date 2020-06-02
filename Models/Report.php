a0a064e5a26e841613ed9bc6787edd25df790d35
Jan Štěch <honza.stech@gmail.com>
2020-06-02 21:20:58 +0200

    Přidání třídy reprezentující objekt hlášení obrázku
    Důvody hlášení jsou nyní napsány v konstantách této třídy
    Vypisování důvodů v reportForm.phtml nyní vypisuje tyto konstanty
    Do třídy Report budou teprve přidány všechny potřebné metody

---
+++ Models/Report.php
@@ -0,0 +1,41 @@
+<?php
+/** 
+ * Třída reprezentující hlášení obrázku
+ * @author Jan Štěch
+ */
+class Report
+{
+    const ADMIN_REQUIRING_REASONS = array(self::REASON_COPYRIGHT, REASON_OTHER_ADMIN);
+    
+    const REASON_NOT_DISPLAYING = 'Obrázek se nezobrazuje správně';
+    const REASON_LONG_LOADING = 'Obrázek se načítá příliš dlouho';
+    const REASON_INCORRECT_NATURAL = 'Obrázek zobrazuje nesprávnou přírodninu';
+    const REASON_CONTAINS_NAME = 'Obrázek obsahuje název přírodniny';
+    const REASON_BAD_RESOLUTION = 'Obrázek má příliš špatné rozlišení';
+    const REASON_COPYRIGHT = 'Obrázek porušuje autorská práva';
+    const REASON_OTHER = 'Jiný důvod (řeší správce třídy)';
+    const REASON_OTHER_ADMIN = 'Jiný důvod (řeší správce služby)';
+    
+    private $id;
+    private $picture;
+    private $reason;
+    private $additionalInformation;
+    private $reportersCount;
+    
+    /**
+     * Konstruktor hlášení nastavující všechny jeho vlastnosti
+     * @param int $id ID hlášení
+     * @param Picture $picture Odkaz na objekt obrázku, kterého se toto hlášení týká
+     * @param string $reason Důvod hlášení (musí být hodnota jedne z konstant této třídy začánající na REASON_)
+     * @param string $additionalInformation Další informace, které může uživatel u určitých hlášení specifikovat
+     * @param int $reportersCount Počet uživatelů, kteří tento obrázek z tohoto důvodu nahlásili
+     */
+    public function __construct(int $id, Picture $picture, string $reason, string $additionalInformation = "", int $reportersCount)
+    {
+        $this->id = $id;
+        $this->picture = $picture;
+        $this->reason = $reason;
+        $this->additionalInformation = $additionalInformation;
+        $this->reportersCount = $reportersCount;
+    }
+}
