6d8c6960ad03bf6bc3c034a34b41d9cdb8152dd6
Jan Štěch <honza.stech@gmail.com>
2020-06-07 23:03:08 +0200

    Přidání JS funkcí pro obsluhu e-mailového formuláře
    Funkce previewEmailMessage a sendMail posílají AJAX POST požadavky na AdministrateActionController, avšak logika pro jejich zpracování zatím není naprogramována
    Ostatní metody pouze modifikují DOM a/nebo upravují proměnnou emailModified
    Pro zobrazení náhledu e-mailu a jeho odeslání se posílají dva AJAX POST požadavky s rozdílnou hodnotou proměnné "action"

--- js/administrate.js
+++ js/administrate.js
@@ -58,6 +58,86 @@ function sixthTab()
 }
 
 /*-------------------------------------------------------*/
+/*-------------------------Tab 5-------------------------*/
+var emailModified = true;	//Proměnná uchovávající informaci o tom, zda byl formulář pro odeslání e-mailu od posledního odeslání modifikován
+function emailModification()
+{
+	emailModified = true;
+}
+function previewEmailMessage()
+{
+	let rawHTMLbody = $("#emailMessage").val();
+	let rawHTMLfooter = $("#emailFooter").val();
+	$.post('administrate-action',
+		{
+			action:"preview email",
+			htmlMessage:rawHTMLbody,
+			htmlFooter:rawHTMLfooter
+		},
+		function(response)
+		{
+			let result = JSON.parse(response)['content'];
+			$("#emailEditor").hide();
+			$("#emailPreviewButton").hide();
+			
+			$("#emailPreview").html(result);
+			$("#emailPreview").show();
+			$("#emailEditButton").show();
+		}
+	);
+}
+function editEmailMessage()
+{
+	$("#emailEditButton").hide();
+	$("#emailPreview").hide();
+	
+	$("#emailEditor").show();
+	$("#emailPreviewButton").show();
+}
+function sendMail()
+{
+	//Ochrana před odesíláním duplicitních e-mailů
+	if (!emailModified)
+	{
+		if (!confirm("Opravdu chcete odeslat ten samý e-mail znovu?"))
+		{
+			return;
+		}
+	}
+	
+	let sender = $("#emailSender").val();
+	let fromAddress = $("#emailSenderAddress").val();
+	let addressee = $("#emailAddressee").val();
+	let subject = $("#emailSubject").val();
+	let rawHTMLbody = $("#emailMessage").val();
+	let rawHTMLfooter = $("#emailFooter").val();
+	
+	$("#statusInfo").show();
+	$("#emailSendButton").attr("disabled", true);
+	
+	$.post('administrate-action',
+		{
+			action:"send email",
+			addressee:addressee,
+			subject:subject,
+			htmlMessage:rawHTMLbody,
+			htmlFooter:rawHTMLfooter,
+			sender:sender,
+			fromAddress:fromAddress
+		},
+		function(response)
+		{
+			$("#statusInfo").hide();
+			$("#emailSendButton").removeAttr("disabled");
+			
+			emailModified = false;
+			
+			let result = JSON.parse(response)['message'];
+			//TODO - nějak šikovněji zobrazit chybovou nebo úspěchovou hlášku
+			alert(result);
+		}
+	);
+}
 /*-------------------------Tab 6-------------------------*/
 function sendSqlQuery()
 {
@@ -69,7 +149,7 @@ function sendSqlQuery()
 		},
 		function(response)
 		{
-			result = JSON.parse(response)['dbResult'];
+			let result = JSON.parse(response)['dbResult'];
 			$("#sqlResult").html(result);
 		}
 	);
