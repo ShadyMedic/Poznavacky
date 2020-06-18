b28bc6e10a5387388d2523a98be94f4e4bae8576
Jan Štěch <honza.stech@gmail.com>
2020-06-19 00:04:54 +0200

    Přro zamítnutí žádosti o změnu jména je nyní nutné zadat důvod.

--- js/administrate.js
+++ js/administrate.js
@@ -86,11 +86,14 @@ function acceptNameChange(event, objectType, requestId)
 }
 function declineNameChange(event, objectType, requestId)
 {
+	let reason = prompt("Zadejte prosím důvod zamítnutí žádosti (uživatel jej obdrží e-mailem, pokud jej zadal). Nevyplnění tohoto pole bude mít za následek zrušení zamítnutí.");
+	if (reason === false || reason.length === 0){ return; }
 	let action = (objectType === "user") ? "decline user name change" : "decline class name change";
 	$.post('administrate-action',
 		{
 			action:action,
 			reqId:requestId,
+			reason: reason
 		},
 		function(response)
 		{
