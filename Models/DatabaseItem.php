23a58bdf0e616f9b19c8a29a77fca39a2e6f3baf
Jan Štěch <honza.stech@gmail.com>
2020-07-28 19:36:15 +0200

    Přidána konstanta obsahující základní hodnoty vlastností do DatabaseItem
    Rovněž je do DatabaseItem přidána konkrétní metoda, která defaultní hodnoty vlastností načítá
    Při konstrukci v databázi dosud neuloženého objektu jsou tyto defaultní hodnoty automaticky načteny

--- Models/DatabaseItem.php
+++ Models/DatabaseItem.php
@@ -6,23 +6,32 @@
 abstract class DatabaseItem
 {
     /**
+     * Název databázové tabulky, která skladuje záznamy typu této třídy
+     * Všechny třídy dědící z této abstraktní třídy musí definovat tuto konstantu
+     * @var string
+     */
+    private const TABLE_NAME = null;
+    
+    /**
+     * Pole defaultních hodnot, které by byly do databázové tabulky nastaveny v případě jejich nespecifikování v SQL INSERT dotazu
+     * Klíče jsou názvy vlastností tohoto objektu, hodnoty jejich defaultní hodnoty
+     * Všechny třídy dědící z této abstraktní třídy musí definovat tuto konstantu
+     * @var array()
+     */
+    private const DEFAULT_VALUES = null;
+    
+    /**
      * TRUE, pokud se jedná o záznam, který dosud není v databázi uložen
      * V takovém případě nemusí mít objekt při volání funkce save() nastavené ID a nelze na něm zavolat funkci load()
      * @var boolean
      */
     protected $savedInDb;
     
-    /**
-     * Název databázové tabulky, která skladuje záznamy typu této třídy
-     * Všechny třídy dědící z této abstraktní třídy musí definovat tuto konstantu
-     * @var string
-     */
-    protected const TABLE_NAME = null;
-    
     protected $id;
     
     /**
      * Konstruktor položky nastavující její ID nebo informaci o tom, že je nová
+     * Pokud se jedná o novou (dosud v databázi neuloženou) položku, jsou do vlastností objektu načteny defaultní hodnoty
      * Tento konstruktor je volán z konstruktorů všech tříd, které z této abstraktní tříd dědí
      * @param bool $isNew FALSE, pokud je již položka se zadaným ID nebo později doplněnými informacemi uložena v databázi, TRUE, pokud se jedná o novou položku
      * @param int $id ID položky (pouze pokud je první argument FALSE)
@@ -33,6 +42,7 @@ abstract class DatabaseItem
         {
             //Nová položka bez známých informací
             $this->savedInDb = false;
+            $this->loadDefaultValues();
         }
         else if (!empty($id))
         {
@@ -49,8 +59,25 @@ abstract class DatabaseItem
     
     /**
      * Metoda nastavující všechny vlastnosti objektu podle proměnných poskytnutých v argumentech
+     * V případě nespecifikování všech argumentů jsou neznámé vlastnosti naplněny základními hodnotami
      */
     public abstract function initialize();
+    
+    /**
+     * Metoda nastavující do vlastností objektu základní hodnoty, které by byly uloženy do databáze i v případě jejich nespecifikování v SQL INSERT dotazu
+     * @param bool $overwriteAll TRUE, pokud mají být základními hodnotami přepsány všechny vlastnosti objektu, FALSE pouze pro přepsání vlastností, jejichž hodnota není nastavena nebo je nastavena na NULL
+     */
+    public function loadDefaultValues(bool $overwriteAll = false)
+    {
+        foreach (self::DEFAULT_VALUES as $fieldName => $fieldValue)
+        {
+            if (!$overwriteAll)
+            {
+                if ($this->$fieldName !== null){ continue; }
+            }
+            $this->$fieldName = $fieldValue;
+        }
+    }
     
     /**
      * Metoda načítající podle údajů poskytnutých v konstruktoru všechny ostatní vlastnosti objektu
@@ -59,6 +86,7 @@ abstract class DatabaseItem
     
     /**
      * Metoda ukládající všechny vlastnosti objektu do databáze, přepisujíce záznam se stejným ID nebo vytvářející nový v případě, že záznam s takovým ID v databázové tabulce neexistuje
+     * Před uložením jsou do nevyplněných vlastností načteny základní hodnoty
      */
     public abstract function save();
     
