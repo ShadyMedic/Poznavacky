a042a2f60aee8279cef55d5dc8087ad52b9f766b
Jan Štěch <honza.stech@gmail.com>
2020-08-17 16:56:59 +0200

    Přidání nového datového typu - undefined
    Pokud je do nějaké vlastnosti modelu uložena instance třídy undefined (záměrně s malým písmenem na začátku), znamená to, že hodnota této vlastnosti zatím nebyla načtena z databáze
    To nyní umožňuje používat hodnotu null pro vlastnosti, které nemusí být v databázi vyplněny (například e-mail uživatele)
    Pokud je tedy vlastnost modelu nastavena na null, znamená to, že byla načtena z databáze, ale hodnota v ní není specifikována
    Instance této třídy bude defaultně přiřazena všem vlastnostem modelů při jejich vytvoření

---
+++ Models/undefined.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Datový typ označující proměnnou nebo vlastnost, do které zatím nebyla přiřazena žádná hodnota
+ * Rozdíl mezi undefined a null je, že v některých případech může být do proměnné přiřazeno null jako platná hodnota, zatímco undefined označuje nenačtenou hodnotu
+ * @author Jan Štěch
+ */
+final class undefined
+{
+    public function __toString()
+    {
+        return 'undefined';
+    }
+}
