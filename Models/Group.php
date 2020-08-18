7527e7976fdbe4f8092c6543f816fc400e221849
Jan Štěch <honza.stech@gmail.com>
2020-08-18 18:11:35 +0200

    Model Group nyní dědí z DatabaseItem a implementuje jeho metody
    Některé metody načítající jednotlivé vlastnosti a gettery jsou upraveny
    Soukromá metoda pro načtení částí poznávačky je nyní veřejná

--- Models/Group.php
+++ Models/Group.php
@@ -3,80 +3,148 @@
  * Třída reprezentující objekt poznávačky obsahující části
  * @author Jan Štěch
  */
-class Group
+class Group extends DatabaseItem
 {
-    private $id;
+    public const TABLE_NAME = 'poznavacky';
+    
+    protected const DEFAULT_VALUES = array(
+        'partsCount' => 0,
+        'parts' => array()
+    );
+    
     private $name;
     private $class;
-    private $partsCount;
-    private $parts;
+    protected $partsCount;
+    
+    protected $parts;
     
     /**
-     * Konstruktor poznávačky nastavující její ID, jméno, třídu, do které patří a počet jejích částí.
-     * Pokud je vše specifikováno, nebude potřeba provádět další SQL dotazy
-     * Pokud je vyplněno jméno i ID, ale chybí nějaký z dalších argumentů, má jméno přednost před ID
-     * @param int $id ID poztnávačky (nepovinné, pokud je specifikováno jméno)
-     * @param string $name Jméno poztnávačky (nepovinné, pokud je specifikováno ID)
-     * @param ClassObject $class Objekt třídy, do které poznávačka patří (nepovinné, v případě nevyplnění bude zjištěno z databáze)
-     * @param int $partsCount Počet částí, které tato poznávačka obsahuje (nepovinné, v případě nevyplnění bude zjištěno z databáze)
-     * @throws AccessDeniedException V případě, že podle ID nebo jména není v databázi nalezena žádná poznávačka
-     * @throws BadMethodCallException V případě, že není specifikován dostatek parametrů
+     * Konstruktor poznávačky nastavující její ID nebo informaci o tom, že je nová
+     * @param bool $isNew FALSE, pokud je již poznávačka se zadaným ID nebo později doplněnými informacemi uložena v databázi, TRUE, pokud se jedná o novou poznávačku
+     * @param int $id ID poznávačky (možné pouze pokud je první argument FALSE; pokud není vyplněno, bude načteno z databáze po vyplnění dalších údajů o ní pomocí metody Group::initialize())
+     * {@inheritDoc}
+     * @see DatabaseItem::initialize()
      */
-    public function __construct(int $id, string $name = "", ClassObject $class = null, int $partsCount = 0)
+    public function __construct(bool $isNew, int $id = 0)
     {
-        if (mb_strlen($name) !== 0 && !empty($id) && !empty($partsCount))
-        {
-            //Je vše je specifikováno --> nastavit
-            $this->id = $id;
-            $this->name = $name;
-            $this->partsCount = $partsCount;
-            $classId = $class->getId();
-        }
-        else if (mb_strlen($name) !== 0)
-        {
-            Db::connect();
-            $result = Db::fetchQuery('SELECT poznavacky_id,casti,tridy_id FROM poznavacky WHERE nazev = ? LIMIT 1',array($name));
-            if (!$result)
-            {
-                //Poznávačka nebyla v databázi nalezena
-                throw new AccessDeniedException(AccessDeniedException::REASON_GROUP_NOT_FOUND);
-            }
-            $id = $result['poznavacky_id'];
-            $this->partsCount = $result['casti'];
-            $classId = $result['tridy_id'];
-        }
-        else if (!empty($id))
-        {
-            Db::connect();
-            $result = Db::fetchQuery('SELECT nazev,casti,tridy_id FROM poznavacky WHERE poznavacky_id = ? LIMIT 1',array($id));
-            if (!$result)
-            {
-                //Poznávačka nebyla v databázi nalezena
-                throw new AccessDeniedException(AccessDeniedException::REASON_GROUP_NOT_FOUND);
-            }
-            $name = $result['nazev'];
-            $this->partsCount = $result['casti'];
-            $classId = $result['tridy_id'];
-        }
-        else
-        {
-            throw new BadMethodCallException('Either ID or name must be specified.', null, null);
-        }
+        parent::__construct($isNew, $id);
+    }
+    
+    /**
+     * Metoda nastavující všechny vlasnosti objektu (s výjimkou ID) podle zadaných argumentů
+     * Při nastavení některého z argumentů na undefined, je hodnota dané vlastnosti také nastavena na undefined
+     * Při nastavení některého z argumentů na null, není hodnota dané vlastnosti nijak pozměněna
+     * @param string|undefined|null $name Název této poznávačky
+     * @param ClassObject|undefined|null $class Odkaz na objekt třídy, do které tato poznávačka patří
+     * @param Part[]|undefined|null $parts Pole částí, jako objekty, na které je tato poznávačka rozdělená
+     * @param int|undefined|null Počet částí, do kterých je tato poznávačka rozdělena (při vyplnění parametru $parts je ignorováno a je použita délka poskytnutého pole)
+     * {@inheritDoc}
+     * @see DatabaseItem::initialize()
+     */
+    public function initialize($name = null, $class = null, $parts = null, $partsCount = null)
+    {
+        //Načtení defaultních hodnot do nenastavených vlastností
+        $this->loadDefaultValues();
         
-        $this->id = $id;
+        //Kontrola nespecifikovaných hodnot (pro zamezení přepsání známých hodnot)
+        if ($name === null){ $name = $this->name; }
+        if ($class === null){ $class = $this->class; }
+        if ($parts === null){ $parts = $this->parts; }
+        if ($partsCount === null){ $partsCount = $this->partsCount; }
+        
         $this->name = $name;
-        
-        //Nastavit nebo zjistit třídu
-        if (!empty($class) && $class->getId() === $classId)
+        $this->class = $class;
+        $this->parts = $parts;
+        $this->partsCount = $partsCount;
+    }
+    
+    /**
+     * Metoda načítající z databáze všechny vlastnosti objektu s výjimkou seznamu částí, do kterých je tato poznávačka rozdělena podle ID (pokud je vyplněno)
+     * Pokud není známé ID této poznávačky, ale je známa třída, do které tato poznávačka patří a název poznávačky, jsou ostatní informace (včetně ID a s výjimkou seznamu částí, do kterých je tato poznávačka rozdělena)
+     * Seznam částí, do kterých je tato poznávačka rozdělena může být načten do vlastnosti Group::$parts pomocí metody Group::loadParts()
+     * @throws BadMethodCallException Pokud se jedná o poznávačku, která dosud není uložena v databázi nebo pokud není o objektu známo dost informací potřebných pro jeho načtení
+     * @throws NoDataException Pokud není poznávačka s odpovídajícími daty nalezena v databázi
+     * @return boolean TRUE, pokud jsou vlastnosti této poznávačky úspěšně načteny z databáze
+     * {@inheritDoc}
+     * @see DatabaseItem::load()
+     */
+    public function load()
+    {
+        if ($this->savedInDb === false)
         {
-            //ID souhlasí a objekt je poskytnut --> nastavit
-            $this->class = $class;
+            throw new BadMethodCallException('Cannot load data about an item that is\'t saved in the database yet');
+        }
+        
+        Db::connect();
+        
+        if ($this->isDefined($this->id))
+        {
+            $result = Db::fetchQuery('SELECT nazev, tridy_id, casti FROM '.self::TABLE_NAME.' WHERE poznavacky_id = ? LIMIT 1', array($this->id));
+            if (empty($result))
+            {
+                throw new NoDataException(NoDataException::UNKNOWN_GROUP);
+            }
+            
+            $name = $result['nazev'];
+            $class = new ClassObject(false, $result['tridy_id']);
+            $partsCount = $result['casti'];
+            $this->initialize($name, $class, $partsCount);
+        }
+        else if ($this->isDefined($this->name) && $this->isDefined($this->class))
+        {
+            $result = Db::fetchQuery('SELECT poznavacky_id, casti FROM '.self::TABLE_NAME.' WHERE nazev = ? AND tridy_id = ? LIMIT 1', array($this->name, $this->class->getId()));
+            if (empty($result))
+            {
+                throw new NoDataException(NoDataException::UNKNOWN_INVITATION);
+            }
+            
+            $this->id = $result['poznavacky_id'];
+            $partsCount = $result['casti'];
+            $this->initialize(null, null, $partsCount);
         }
         else
         {
-            //Objekt není poskytnut, nebo nesouhlasí ID --> vytvořit
-            $this->class = new ClassObject(false, $classId);
+            throw new BadMethodCallException('Not enough properties are know about the item to be able to load the rest');
         }
+        
+        return true;
+    }
+    
+    /**
+     * Metoda ukládající data této poznávačky do databáze
+     * Pokud se jedná o novou poznávačku (vlastnost $savedInDb je nastavena na FALSE), je vložen nový záznam
+     * V opačném případě jsou přepsána data poznávačky se stejným ID
+     * @throws BadMethodCallException Pokud se nejedná o novou poznávačku a zároveň není známo jeho ID (znalost ID poznávačky je nutná pro modifikaci databázové tabulky)
+     * @return boolean TRUE, pokud je poznávačka úspěšně uložena do databáze
+     * {@inheritDoc}
+     * @see DatabaseItem::save()
+     */
+    public function save()
+    {
+        if ($this->savedInDb === true && !$this->isDefined($this->id))
+        {
+            throw new BadMethodCallException('ID of the item must be loaded before saving into the database, since this item isn\'t new');
+        }
+        
+        Db::connect();
+        if ($this->savedInDb)
+        {
+            //Aktualizace existující poznávačky
+            $this->loadIfNotAllLoaded();
+            
+            $result = Db::executeQuery('UPDATE '.self::TABLE_NAME.' SET nazev = ?, tridy_id = ?, casti = ? WHERE poznavacky_id = ? LIMIT 1', array($this->name, $this->class->getId(), $this->partsCount, $this->id));
+        }
+        else
+        {
+            //Tvorba nové poznávačky
+            $this->id = Db::executeQuery('INSERT INTO '.self::TABLE_NAME.' (nazev,tridy_id,casti) VALUES (?,?,?)', array($this->name, $this->class->getId(), $this->partsCount), true);
+            if (!empty($this->id))
+            {
+                $this->savedInDb = true;
+                $result = true;
+            }
+        }
+        return $result;
     }
     
     /**
@@ -85,6 +153,7 @@ class Group
      */
     public function getId()
     {
+        $this->loadIfNotLoaded($this->id);
         return $this->id;
     }
     
@@ -94,6 +163,7 @@ class Group
      */
     public function getName()
     {
+        $this->loadIfNotLoaded($this->name);
         return $this->name;
     }
     
@@ -103,6 +173,7 @@ class Group
      */
     public function getClass()
     {
+        $this->loadIfNotLoaded($this->class);
         return $this->class;
     }
     
@@ -112,15 +183,17 @@ class Group
      */
     public function getPartsCount()
     {
+        $this->loadIfNotLoaded($this->partsCount);
         return $this->partsCount;
     }
     
     /**
-     * Metoda navracející objekt náhodně zvoleného obrázku z nějaké části této poznávačky
+     * Metoda navracející pole náhodně zvolených obrázků z nějaké části této poznávačky jako objekty
      * Šance na výběr části je přímo úměrná počtu přírodnin, které obsahuje
      * Všechny přírodniny této poznávačky tak mají stejnou šanci, že jejich obrázek bude vybrán
      * Počet obrázků u jednotlivých přírodniny nemá na výběr vliv
      * @param int $count Požadovaný počet náhodných obrázků (není zajištěna absence duplikátů)
+     * @return Picture[] Polé náhodně vybraných obrázků obsahující specifikovaný počet prvků
      */
     public function getRandomPictures(int $count)
     {
@@ -144,10 +217,7 @@ class Group
      */
     public function getNaturals()
     {
-        if (!isset($this->parts))
-        {
-            $this->loadParts();
-        }
+        if (!$this->isDefined($this->parts)){ $this->loadParts(); }
         
         $allPartsIds = array();
         foreach ($this->parts as $part)
@@ -174,7 +244,7 @@ class Group
      */
     public function getParts()
     {
-        if (!isset($this->parts))
+        if (!$this->isDefined($this->parts))
         {
             $this->loadParts();
         }
@@ -184,9 +254,9 @@ class Group
     /**
      * Metoda načítající části patřící do této poznávačky a ukládající je jako vlastnost
      */
-    private function loadParts()
+    public function loadParts()
     {
-        $this->parts = array();
+        $this->loadIfNotLoaded($this->id);
         
         Db::connect();
         $result = Db::fetchQuery('SELECT casti_id,nazev,prirodniny,obrazky FROM casti WHERE poznavacky_id = ?', array($this->id), true);
@@ -197,6 +267,7 @@ class Group
         }
         else
         {
+            $this->parts = array();
             foreach ($result as $partData)
             {
                 $this->parts[] = new Part($partData['casti_id'], $partData['nazev'], $this, $partData['prirodniny'], $partData['obrazky']);
@@ -211,10 +282,7 @@ class Group
      */
     private function getPartById(int $id)
     {
-        if (!isset($this->parts))
-        {
-            $this->loadParts();
-        }
+        if (!$this->isDefined($this->parts)){ $this->loadParts(); }
         foreach ($this->parts as $part)
         {
             if ($part->getId() === $id)
@@ -222,5 +290,22 @@ class Group
                 return $part;
             }
         }
+    }
+    
+    /**
+     * Metoda odstraňující tuto poznávačku z databáze
+     * @return boolean TRUE, pokud je poznávačka úspěšně odstraněna z databáze
+     * {@inheritDoc}
+     * @see DatabaseItem::delete()
+     */
+    public function delete()
+    {
+        $this->loadIfNotLoaded($this->id);
+        
+        Db::connect();
+        Db::executeQuery('DELETE FROM '.self::TABLE_NAME.' WHERE poznavacky_id = ? LIMIT 1;', array($this->id));
+        $this->id = new undefined();
+        $this->savedInDb = false;
+        return true;
     }
 }
