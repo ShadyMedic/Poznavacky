1bc45eee36c731bf5cbaab17fa9057c07c068882
Jan Štěch <honza.stech@gmail.com>
2020-08-21 22:39:21 +0200

    Model Picture nyní dědí z DatabaseItem a implementuje jeho metody
    Gettery nyní před navrácením hodnoty ověřují, zda je daná vlastnost načtena a případně volají Picture::load()
    Soukromá metoda pro načtení hlášení obrázku je nyní veřejná
    Metoda Picture::checkReportsLoaded je odstraněna
    Metoda Picture::delete() je přepsána

--- Models/Picture.php
+++ Models/Picture.php
@@ -3,55 +3,166 @@
  * Třída reprezentující objekt obrázku
  * @author Jan Štěch
  */
-class Picture
+class Picture extends DatabaseItem
 {
-    private $id;
+    public const TABLE_NAME = 'prirodniny';
+    
+    protected const DEFAULT_VALUES = array(
+        'enabled' => true
+    );
+    
     private $src;
     private $natural;
+    private $part;
     private $enabled;
+    
     private $reports;
     
     /**
-     * Konstruktor obrázku nastavující jeho vlastnosti
-     * @param int $id ID obrázku
-     * @param string $url Zdroj obrázku (nepovinné, pokud je zadáno ID)
-     * @param Natural $natural Objekt přírodniny na obrázku (nepovinné, pokud je zadáno ID)
-     * @param bool $enabled Stav obrázku (TRUE, pokud je povolen, FALSE, pokud ne; nepovinné, pokud je zadáno ID)
+     * Konstruktor obrázku nastavující jeho ID nebo informaci o tom, že je nový
+     * @param bool $isNew FALSE, pokud je již obrázek se zadaným ID nebo později doplněnými informacemi uložen v databázi, TRUE, pokud se jedná o nový obrázek
+     * @param int $id ID obrázku (možné pouze pokud je první argument FALSE; pokud není vyplněno, bude načteno z databáze po vyplnění dalších údajů o ní pomocí metody Picture::initialize())
+     * {@inheritDoc}
+     * @see DatabaseItem::initialize()
      */
-    public function __construct(int $id, string $url = "", Natural $natural = null, bool $enabled = null)
+    public function __construct(bool $isNew, int $id = 0)
     {
-        $this->id = $id;
-        $this->src = $url;
-        $this->natural = $natural;
-        $this->enabled = $enabled;
+        parent::__construct($isNew, $id);
+    }
+    
+    /**
+     * Metoda nastavující všechny vlasnosti objektu (s výjimkou ID) podle zadaných argumentů
+     * Při nastavení některého z argumentů na undefined, je hodnota dané vlastnosti také nastavena na undefined
+     * Při nastavení některého z argumentů na null, není hodnota dané vlastnosti nijak pozměněna
+     * @param string|undefined|null $src Adresa, pod kterou lze obrázek najít
+     * @param Natural|undefined|null $natural Odkaz na objekt přírodniny, kterou tento obrázek zobrazuje
+     * @param Part|undefined|null $part Odkaz na objekt části, ve které se obrázek v současnosti nachází
+     * @param bool|undefined|null $enabled TRUE, pokud je obrázek povolen, FALSE, pokud je skryt
+     * @param Report[]|undefined|null $reports Pole hlášení tohoto obrázku, jako objekty
+     * {@inheritDoc}
+     * @see DatabaseItem::initialize()
+     */
+    public function initialize($src = null, $natural = null, $part = null, $enabled = null, $reports = null)
+    {
+        //Načtení defaultních hodnot do nenastavených vlastností
+        $this->loadDefaultValues();
         
-        if (mb_strlen($url) !== 0 && !empty($id) && !empty($natural) && isset($enabled))
+        //Kontrola nespecifikovaných hodnot (pro zamezení přepsání známých hodnot)
+        if ($src === null){ $src = $this->src; }
+        if ($natural === null){ $natural = $this->natural; }
+        if ($part === null){ $part = $this->part; }
+        if ($enabled === null){ $enabled = $this->enabled; }
+        if ($reports === null){ $reports = $this->reports; }
+        
+        $this->src = $src;
+        $this->natural = $natural;
+        $this->part = $part;
+        $this->enabled = $enabled;
+        $this->reports = $reports;
+    }
+    
+    /**
+     * Metoda načítající z databáze všechny vlastnosti objektu s výjimkou seznamu hlášení tohoto obrázku, podle ID (pokud je vyplněno)
+     * V případě, že není známé ID, ale je známá adresa obrázku a přírodnina nebo část, do které patří, jsou načteny ty samé informace + ID podle těchto známých informací
+     * Hlášení tohoto obrázku lze načíst samostatně pomocí metody Picture::loadReports()
+     * @throws BadMethodCallException Pokud se jedná o obrázek, který dosud není uložen v databázi nebo pokud není o objektu známo dost informací potřebných pro jeho načtení
+     * @throws NoDataException Pokud není obrázek nalezen v databázi
+     * @return boolean TRUE, pokud jsou vlastnosti tohoto obrázku úspěšně načteny z databáze
+     * {@inheritDoc}
+     * @see DatabaseItem::load()
+     */
+    public function load()
+    {
+        if ($this->savedInDb === false)
         {
-            //Vše je vyplněno --> nic nezjišťovat
+            throw new BadMethodCallException('Cannot load data about an item that is\'t saved in the database yet');
         }
-        else if (!empty($id))
+        
+        Db::connect();
+        
+        if ($this->isDefined($this->id))
         {
-            //Je vyplněno ID, ale něco z dalších informací chybí --> načíst z databáze
-            Db::connect();
-            $result = Db::fetchQuery('SELECT prirodniny_id,zdroj,povoleno FROM obrazky WHERE obrazky_id = ? LIMIT 1',array($id));
-            if (!$result)
+            $result = Db::fetchQuery('SELECT zdroj, prirodniny_id, casti_id, povoleno FROM '.self::TABLE_NAME.' WHERE obrazky_id = ? LIMIT 1', array($this->id));
+            if (empty($result))
             {
-                //Obrázek nebyl v databázi nalezena
-                throw new AccessDeniedException(AccessDeniedException::REASON_PICTURE_NOT_FOUND);
+                throw new NoDataException(NoDataException::UNKNOWN_PICTURE);
             }
-            $url = $result['zdroj'];
+            
+            $src = $result['zdroj'];
             $natural = new Natural(false, $result['prirodniny_id']);
-            $enabled = ($result['povoleno'] == 1) ? true : false;
+            $part = new Part(false, $result['casti_id']);
+        }
+        else if ($this->isDefined($this->src) && $this->isDefined($this->natural))
+        {
+            $result = Db::fetchQuery('SELECT obrazky_id, casti_id, povoleno FROM '.self::TABLE_NAME.' WHERE zdroj = ? AND prirodniny_id = ? LIMIT 1', array($this->src, $this->natural->getId()));
+            if (empty($result))
+            {
+                throw new NoDataException(NoDataException::UNKNOWN_PICTURE);
+            }
+            $this->id = $result['obrazky_id'];
+            $src = null;
+            $natural = null;
+            $part = new Part(false, $result['casti_id']);
+        }
+        else if ($this->isDefined($this->src) && $this->isDefined($this->part))
+        {
+            $result = Db::fetchQuery('SELECT obrazky_id, prirodniny_id, povoleno FROM '.self::TABLE_NAME.' WHERE zdroj = ? AND casti_id = ? LIMIT 1', array($this->src, $this->part->getId()));
+            if (empty($result))
+            {
+                throw new NoDataException(NoDataException::UNKNOWN_PICTURE);
+            }
+            $this->id = $result['obrazky_id'];
+            $src = null;
+            $natural = new Natural(false, $result['prirodniny_id']);
+            $part = null;
         }
         else
         {
-            throw new BadMethodCallException('ID of the picture must be specified.', null, null);
+            throw new BadMethodCallException('Not enough properties are know about the item to be able to load the rest');
         }
         
-        $this->id = $id;
-        $this->src = $url;
-        $this->natural = $natural;
-        $this->enabled = $enabled;
+        $enabled = ($result['povoleno'] === 1) ? true : false;
+        
+        $this->initialize($src, $natural, $part, $enabled, null);
+        
+        return true;
+    }
+    
+    /**
+     * Metoda ukládající data tohoto obrázku do databáze
+     * Pokud se jedná o nový obrázek (vlastnost $savedInDb je nastavena na FALSE), je vložen nový záznam
+     * V opačném případě jsou přepsána data obrázku se stejným ID
+     * @throws BadMethodCallException Pokud se nejedná o nový obrázek a zároveň není známo jeho ID (znalost ID obrázku je nutná pro modifikaci databázové tabulky)
+     * @return boolean TRUE, pokud je obrázek úspěšně uložen do databáze
+     * {@inheritDoc}
+     * @see DatabaseItem::save()
+     */
+    public function save()
+    {
+        if ($this->savedInDb === true && !$this->isDefined($this->id))
+        {
+            throw new BadMethodCallException('ID of the item must be loaded before saving into the database, since this item isn\'t new');
+        }
+        
+        Db::connect();
+        if ($this->savedInDb)
+        {
+            //Aktualizace existujícího obrázku
+            $this->loadIfNotAllLoaded();
+            
+            $result = Db::executeQuery('UPDATE '.self::TABLE_NAME.' SET zdroj = ?, prirodniny_id = ?, casti_id = null, povoleno = ? WHERE obrazky_id = ? LIMIT 1', array($this->src, $this->natural->getId(), $this->part->getId(), $this->enabled, $this->id));
+        }
+        else
+        {
+            //Tvorba nové pozvánky
+            $this->id = Db::executeQuery('INSERT INTO '.self::TABLE_NAME.' (zdroj,prirodniny_id,casti_id,povoleno) VALUES (?,?,?,?)', array($this->src, $this->natural->getId(), $this->part->getId(), $this->enabled), true);
+            if (!empty($this->id))
+            {
+                $this->savedInDb = true;
+                $result = true;
+            }
+        }
+        return $result;
     }
     
     /**
@@ -60,6 +171,7 @@ class Picture
      */
     public function getId()
     {
+        $this->loadIfNotDefined($this->id);
         return $this->id;
     }
     
@@ -69,6 +181,7 @@ class Picture
      */
     public function getSrc()
     {
+        $this->loadIfNotDefined($this->src);
         return $this->src;
     }
     
@@ -78,6 +191,7 @@ class Picture
      */
     public function getNatural()
     {
+        $this->loadIfNotDefined($this->natural);
         return $this->natural;
     }
     
@@ -87,9 +201,11 @@ class Picture
      */
     public function isEnabled()
     {
+        $this->loadIfNotDefined($this->enabled);
         return $this->enabled;
     }
     
+    //TODO - upravit metodu tak, aby jako první parametr brala proměnnou typu Natural
     /**
      * Metoda upravující přírodninu a adresu tohoto obrázku z rozhodnutí administrátora
      * @param Natural $newNatural Objekt reprezentující nově zvolenou přírodninu
@@ -104,6 +220,9 @@ class Picture
         {
             throw new AccessDeniedException(AccessDeniedException::REASON_INSUFFICIENT_PERMISSION);
         }
+        
+        $this->loadIfNotDefined($this->natural);
+        $this->loadIfNotDefined($this->id);
         
         //Kontrola, zda daná nová URL adresa vede na obrázek a zda je nová přírodnina součástí té samé poznávačky, jako ta stará
         $checker = new PictureAdder($this->natural->getPart()->getGroup());
@@ -142,22 +261,14 @@ class Picture
      */
     public function getReports()
     {
-        $this->checkReportsLoaded();
+        if (!$this->isDefined($this->reports)){ $this->loadReports(); }
         return $this->reports;
-    }
-    
-    private function checkReportsLoaded()
-    {
-        if (!isset($this->reports))
-        {
-            $this->loadReports();
-        }
     }
     
     /**
      * Metoda načítající hlášení tohoto obrázku z databáze a ukládající je do vlastnosti této instance jako objekty
      */
-    private function loadReports()
+    public function loadReports()
     {
         Db::connect();
         $result = Db::fetchQuery('SELECT hlaseni_id,duvod,dalsi_informace,pocet FROM hlaseni WHERE obrazky_id = ?', array($this->id), true);
@@ -192,6 +303,8 @@ class Picture
         
         //Kontrola dat OK
         
+        $this->loadIfNotDefined($this->id);
+        
         //Vypnout obrázek v databázi
         Db::connect();
         Db::executeQuery('UPDATE obrazky SET povoleno = 0 WHERE obrazky_id = ? LIMIT 1;', array($this->id));
@@ -202,32 +315,18 @@ class Picture
     
     /**
      * Metoda odstraňující tento obrázek z databáze
-     * Vlastnosti této instance jsou vynulovány
-     * Insance, na níž je vykonána tato metoda by měla být okamžitě zničena pomocí unset()
-     * Tato metoda může být použita pouze v případě, že právě přihlášený uživatel je systémový administrátor
-     * @throws AccessDeniedException Pokud není přihlášený uživatel administrátorem
      * @return boolean TRUE, pokud je obrázek úspěšně odstraněn z databáze
+     * {@inheritDoc}
+     * @see DatabaseItem::delete()
      */
     public function delete()
     {
-        //Kontrola, zda je právě přihlášený uživatelem administrátorem
-        if (!AccessChecker::checkSystemAdmin())
-        {
-            throw new AccessDeniedException(AccessDeniedException::REASON_INSUFFICIENT_PERMISSION);
-        }
-        
-        //Kontrola dat OK
-        
-        //Odstranit obrázek z databáze
-        Db::connect();
-        Db::executeQuery('DELETE FROM obrazky WHERE obrazky_id = ? LIMIT 1;', array($this->id));
-        
-        //Vymazat data z této instance hlášení
-        $this->id = null;
-        $this->src = null;
-        $this->natural = null;
-        $this->enabled = null;
-        
-        return true;
+    	$this->loadIfNotLoaded($this->id);
+    	
+    	Db::connect();
+    	Db::executeQuery('DELETE FROM '.self::TABLE_NAME.' WHERE obrazky_id = ? LIMIT 1;', array($this->id));
+    	$this->id = new undefined();
+    	$this->savedInDb = false;
+    	return true;
     }
 }
