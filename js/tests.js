6f353e10b4ee50b07789131beb3730d68580c6c6
Jan Štěch <honza.stech@gmail.com>
2020-10-26 16:15:06 +0100

    Rozdělení JavaScriptu v manage.js na members.js a tests.js
    Skript po správu obecných informací zůstává v manage.js.
    Skript obsluhující tabulku pro správu poznávaček je přesunut do tests.js.
    Skript obsluhující tabulka pro správu členů je přesunut do members.js.
    Skripty zatím nebyly příliš testovány, ale neměly by nastat žádné větší problémy. Na chyby by se časem určitě přislo.

---
+++ js/tests.js
@@ -0,0 +1,66 @@
+var deletedTableRow;    //Ukládá řádek tabulky potnávaček, který je odstraňován
+
+function createTest()
+{
+	$("#createButton").hide();
+	$("#createForm").show();
+}
+function createTestHide()
+{
+	$("#createInput").val("");
+	$("#createForm").hide();
+	$("#createButton").show();
+}
+function createTestSubmit()
+{
+	var testName = $("#createInput").val();
+	$.post("class-update",
+		{
+    		action: 'create test',
+			testName: testName
+		},
+		function (response)
+		{
+			response = JSON.parse(response);
+			if (response["messageType"] === "error")
+			{
+				//TODO - zobraz nějak chybovou hlášku - ideálně ne jako alert() nebo jiný popup
+				alert(response["message"]);
+			}
+			else if (response["messageType"] === "success")
+			{
+				//Znovu načti stránku, ať se zobrazí nová poznávačka v DOM
+				location.reload();
+			}
+		}
+	);
+}
+/*-------------------------------------------------------*/
+function deleteTest(testId, name)
+{
+    if (!confirm("Opravdu chcete trvale odstranit poznávačku " + name + "? Přírodniny, které tato poznávačka obsahuje ani jejich obrázky nebudou odstraněny, ale zůstanou nepřiřazeny, dokud je nepřidáte do jiné poznávačky. Tato akce je nevratná!"))
+    {
+    	return;
+	}
+	deletedTableRow = event.target.parentNode.parentNode.parentNode;
+	$.post("class-update",
+		{
+    		action: 'delete test',
+			testId: testId
+		},
+		function (response)
+		{
+			response = JSON.parse(response);
+			if (response["messageType"] === "error")
+			{
+				//TODO - zobraz nějak chybovou hlášku - ideálně ne jako alert() nebo jiný popup
+				alert(response["message"]);
+			}
+			else if (response["messageType"] === "success")
+			{
+				deletedTableRow.remove();
+			}
+			deletedTableRow = undefined;
+		}
+	);
+}
