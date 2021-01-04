cc8c53615a4c4ca4062ef78834974e8ca7e753b5
eksyska <eli.korcova@gmail.com>
2021-01-04 10:28:11 +0100

    Zpřehlednění index.js

--- js/index.js
+++ js/index.js
@@ -4,62 +4,64 @@
 
 //zpracování eventů
 $(function() { 
+	//zobrazení cookies alertu
 	setTimeout(() => {
 		$("#cookies-alert").addClass("show");
 	}, 1000);
 
-	$(window).resize(function() {
-	})
-
-	//zasunutí cookies alertu
-	$("#hide-cookies-alert-button").click(function(){
-		$("#cookies-alert").removeClass("show");
-	})
-
-	//skrytí login sekce kliknutím na tlačítko
-	$("#hide-login-section-button").click(function(){hideLoginSection();})
+	//event listenery tlačítek
+	$("#hide-login-section-button").click(function(){hideLoginSection()})
+	$("#hide-cookies-alert-button").click(function(){hideCookiesAlert()})
+	$(".show-login-section-login-button, .show-login-section-register-button, .show-login-section-password-recovery-button").click(function(event){showLoginSection(event)});
 	
-	//skrytí login sekce tím, že se klikne mimo
-	$(document).mouseup(function(e) 
-	{
-		var container = $("#index-login-section");
-		var cookiesAlert = $("#cookies-alert");
+	//event listener kliknutí myši
+	$(document).mouseup(function(e) {mouseUpChecker(e)})
 
-		if (!container.is(e.target) && !cookiesAlert.is(e.target) && container.has(e.target).length === 0 && cookiesAlert.has(e.target).length === 0) 
-		{
-			hideLoginSection();
-		}
-	});
+	//event listener scrollování
+	$(window).scroll(function(e) {showScrollButton(e)})
+})
 
-	//zobrazení login sekce
-	$(".show-login-section-login-button, .show-login-section-register-button, .show-login-section-password-recovery-button").click(function(event){
-		if(!$("#index-login-section").hasClass("show")) {
-			$("#index-login-section").addClass("show");
-			$(".overlay").addClass("show");
-			$("body").css("overflowY", "hidden");	
-		}
-		if ($(event.target).hasClass("show-login-section-login-button"))
-			showLoginDiv('login');
-		else if ($(event.target).hasClass("show-login-section-register-button"))
-			showLoginDiv('register');
-		else if ($(event.target).hasClass("show-login-section-password-recovery-button"))
-			showLoginDiv('password-recovery');
-	})
 
-	//zobrazení/skrytí back-to-top tlačítka podle toho, kolik je odscrollováno
-	var documentHeight = $(window).height();
-	var scrollOffset = 50;
-	$(window).scroll(function(event) {
-		var scrolled = $(window).scrollTop();
-		console.log(scrolled);
-		if (scrolled > (documentHeight + scrollOffset)) {
-			$("#back-to-top").addClass("show");
-		}
-		else if (scrolled <= (documentHeight + scrollOffset)) {
-			$("#back-to-top").removeClass("show");
-		}
-	})
-});
+//zasunutí cookies alertu
+function hideCookiesAlert(){
+	$("#cookies-alert").removeClass("show");
+}
+
+//zobrazení/skrytí back-to-top tlačítka podle toho, kolik je odscrollováno
+var documentHeight = $(window).height();
+var scrollOffset = 50;
+function showScrollButton(event) {
+	var scrolled = $(window).scrollTop();
+	if (scrolled > (documentHeight + scrollOffset)) {
+		$("#back-to-top").addClass("show");
+	}
+	else if (scrolled <= (documentHeight + scrollOffset)) {
+		$("#back-to-top").removeClass("show");
+	}
+}
+
+//zobrazení login sekce
+function showLoginSection(e) {
+	if(!$("#index-login-section").hasClass("show")) {
+		$("#index-login-section").addClass("show");
+		$(".overlay").addClass("show");
+		$("body").css("overflowY", "hidden");	
+	}
+	if ($(e.target).hasClass("show-login-section-login-button"))
+		showLoginDiv('login');
+	else if ($(e.target).hasClass("show-login-section-register-button"))
+		showLoginDiv('register');
+	else if ($(e.target).hasClass("show-login-section-password-recovery-button"))
+		showLoginDiv('password-recovery');
+}
+
+//zobrazení požadované části v login sekci
+function showLoginDiv(divId) {
+	$("#register").hide();
+	$("#login").hide();
+	$("#password-recovery").hide();
+	$("#" + divId).show();
+}
 
 //skrytí login sekce
 function hideLoginSection() {
@@ -76,12 +78,15 @@ function emptyForms(fields) {
 	formTextFields.val('');
 }
 
-//zobrazení požadované části v login sekci
-function showLoginDiv(divId) {
-	$("#register").hide();
-	$("#login").hide();
-	$("#password-recovery").hide();
-	$("#" + divId).show();
+//detekce kliknutí mimo login sekci
+function mouseUpChecker(e) {
+	var container = $("#index-login-section");
+	var cookiesAlert = $("#cookies-alert");
+
+	if (!container.is(e.target) && !cookiesAlert.is(e.target) && container.has(e.target).length === 0 && cookiesAlert.has(e.target).length === 0) 
+	{
+		hideLoginSection();
+	}
 }
 
 /*--------------------------------------------------------------------------*/
