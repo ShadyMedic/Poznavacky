ef25e01aeff8172fcdceba9eb932a53e24fcca8d
Jan Štěch <honza.stech@gmail.com>
2021-01-17 16:48:05 +0100

    Odebrání neexistujícího .js souboru ze seznamu skriptů pro tests stránku

--- Controllers/Menu/Management/ClassObject/TestsController.php
+++ Controllers/Menu/Management/ClassObject/TestsController.php
@@ -107,9 +107,9 @@ class TestsController extends Controller
             $this->pageHeader['description'] = 'Nástroj pro správce tříd umožnňující snadnou správu poznávaček';
             $this->pageHeader['keywords'] = '';
             $this->pageHeader['cssFiles'] = array('css/css.css');
-            $this->pageHeader['jsFiles'] = array('js/generic.js', 'js/menu', 'js/ajaxMediator.js','js/tests.js');
+            $this->pageHeader['jsFiles'] = array('js/generic.js', 'js/ajaxMediator.js','js/tests.js');
             $this->pageHeader['bodyId'] = 'tests';
-            
+
             $this->data['baseUrl'] = 'menu/'.$_SESSION['selection']['class']->getUrl().'/manage/tests';
             $this->data['groups'] = $_SESSION['selection']['class']->getGroups();
             $this->data['returnButtonLink'] = 'menu/'.$_SESSION['selection']['class']->getUrl().'/manage';
