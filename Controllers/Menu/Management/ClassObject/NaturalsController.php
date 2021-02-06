7e114da4cd6230a26c67dca410ca05c769c57399
Jan Štěch <honza.stech@gmail.com>
2021-02-06 13:05:23 +0100

    Přidání stránky naturals umožňující správu jednotlivých přírodnin
    Tato stránka je na stejné úrovni jako stránky tests a members.
    Tlačítko pro vstup na tuto stránku je již z minulých commitů umístněno na stránce manage.
    Stránka je logicky přístupná pouze správcům třídy a systémovým administrátorům.
    Stránka zatím není nastylována a chybí jí JavaScript zprostředkovávající funkčnost.

---
+++ Controllers/Menu/Management/ClassObject/NaturalsController.php
@@ -0,0 +1,35 @@
+<?php
+namespace Poznavacky\Controllers\Menu\Management\ClassObject;
+
+use Poznavacky\Controllers\Controller;
+
+/**
+ * Kontroler starající se o výpis stránky pro správu přírodnin třídy jejím správcům
+ * @author Jan Štěch
+ */
+class NaturalsController extends Controller
+{
+
+    /**
+     * Metoda nastavující hlavičku stránky, data pro pohled a pohled
+     * @see Controller::process()
+     */
+    function process(array $parameters): void
+    {
+        $this->pageHeader['title'] = 'Správa přírodnin';
+        $this->pageHeader['description'] = 'Nástroj pro správce tříd umožňující snadnou správu přírodnin';
+        $this->pageHeader['keywords'] = '';
+        $this->pageHeader['cssFiles'] = array('css/css.css');
+        $this->pageHeader['jsFiles'] = array('js/generic.js', 'js/menu.js', 'js/ajaxMediator.js','js/naturals.js');
+        $this->pageHeader['bodyId'] = 'naturals';
+        $this->data['navigationBar'] = array(
+            0 => array(
+                'text' => $this->pageHeader['title'],
+                'link' => 'menu/'.$_SESSION['selection']['class']->getUrl().'/manage/naturals'
+            )
+        );
+        $this->data['naturals'] = $_SESSION['selection']['class']->getNaturals();
+
+        $this->view = 'naturals';
+    }
+}
