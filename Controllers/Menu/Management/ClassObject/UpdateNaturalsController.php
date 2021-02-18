45c728e40d5cff59f73bf32eaf452b3dee03b7cc
Jan Štěch <honza.stech@gmail.com>
2021-02-18 18:02:58 +0100

    Propojení kontroleru UpdateNaturalsController s modelem NaturalsEditor
    Akce prováděné na stránce naturals jsou nyní zaznamenávány a ukládány do databáze

--- Controllers/Menu/Management/ClassObject/UpdateNaturalsController.php
+++ Controllers/Menu/Management/ClassObject/UpdateNaturalsController.php
@@ -2,7 +2,9 @@
 namespace Poznavacky\Controllers\Menu\Management\ClassObject;
 
 use Poznavacky\Controllers\Controller;
+use Poznavacky\Models\DatabaseItems\Natural;
 use Poznavacky\Models\Exceptions\AccessDeniedException;
+use Poznavacky\Models\Processors\NaturalEditor;
 use Poznavacky\Models\Security\AccessChecker;
 use Poznavacky\Models\Statics\UserManager;
 use Poznavacky\Models\AjaxResponse;
@@ -24,7 +26,7 @@ class UpdateNaturalsController extends Controller
             header('HTTP/1.0 400 Bad Request');
             exit();
         }
-        
+
         header('Content-Type: application/json');
         //Kontrola, zda je zvolena nějaká třída
         if (!isset($_SESSION['selection']['class']))
@@ -34,7 +36,7 @@ class UpdateNaturalsController extends Controller
             exit();
         }
         $class = $_SESSION['selection']['class'];
-        
+
         //Kontrola, zda je nějaký uživatel přihlášen a zda je přihlášený uživatel správcem vybrané třídy nebo systémový administrátor
         $aChecker = new AccessChecker();
         if (!$aChecker->checkUser() || !($class->checkAdmin(UserManager::getId()) || $aChecker->checkSystemAdmin()))
@@ -42,28 +44,34 @@ class UpdateNaturalsController extends Controller
             header('HTTP/1.0 403 Forbidden');
             exit();
         }
-        
+
         try
         {
+            $editor = new NaturalEditor($class);
+
             switch ($_POST['action'])
             {
                 case 'rename':
                     $naturalId = $_POST['naturalId'];
                     $newName = $_POST['newName'];
-                    //TODO
+                    $natural = new Natural(false, $naturalId);
+                    $editor->rename($natural, $newName);
                     $response = new AjaxResponse(AjaxResponse::MESSAGE_TYPE_SUCCESS, 'Přírodnina úspěšně přejmenována');
                     echo $response->getResponseString();
                     break;
                 case 'merge':
                     $fromNaturalId = $_POST['fromNaturalId'];
                     $toNaturalId = $_POST['toNaturalId'];
-                    //TODO
+                    $fromNatural = new Natural(false, $fromNaturalId);
+                    $toNatural = new Natural(false, $toNaturalId);
+                    $editor->merge($fromNatural, $toNatural);
                     $response = new AjaxResponse(AjaxResponse::MESSAGE_TYPE_SUCCESS, 'Přírodniny úspěšně sloučeny a obrázky převedeny');
                     echo $response->getResponseString();
                     break;
                 case 'delete':
                     $naturalId = $_POST['naturalId'];
-                    //TODO
+                    $natural = new Natural(false, $naturalId);
+                    $editor->delete($natural);
                     $response = new AjaxResponse(AjaxResponse::MESSAGE_TYPE_SUCCESS, 'Přírodnina úspěšně odstraněna');
                     echo $response->getResponseString();
                     break;
@@ -77,7 +85,7 @@ class UpdateNaturalsController extends Controller
             $response = new AjaxResponse(AjaxResponse::MESSAGE_TYPE_ERROR, $e->getMessage(), array('origin' => $_POST['action']));
             echo $response->getResponseString();
         }
-        
+
         //Zastav zpracování PHP, aby se nevypsala šablona
         exit();
     }
