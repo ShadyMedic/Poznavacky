0a5170f2abc77df0c5cc1998b38182977b70431c
Jan Štěch <honza.stech@gmail.com>
2021-02-25 13:12:49 +0100

    Přidání logování, jednoho catch bloku, vyčištění kódu, oprava překlepu
    Změny jsou provedeny v kontroleru RecoverPasswordController

--- Controllers/RecoverPasswordController.php
+++ Controllers/RecoverPasswordController.php
@@ -3,6 +3,8 @@ namespace Poznavacky\Controllers;
 
 use Poznavacky\Models\DatabaseItems\User;
 use Poznavacky\Models\Exceptions\AccessDeniedException;
+use Poznavacky\Models\Exceptions\DatabaseException;
+use Poznavacky\Models\Logger;
 use Poznavacky\Models\MessageBox;
 use Poznavacky\Models\PasswordRecoveryCodeVerificator;
 use Poznavacky\Models\Statics\Db;
@@ -15,6 +17,7 @@ class RecoverPasswordController extends Controller
 {
     /**
      * Metoda ověřující, zda je zadán platný kód pro obnovu hesla, nastavující hlavičku stránky a pohled
+     * @param array $parameters Parametry pro kontroler; první element by měl obsahovat kód pro obnovu hesla
      * @see Controller::process()
      */
     public function process(array $parameters): void
@@ -25,24 +28,28 @@ class RecoverPasswordController extends Controller
         $this->pageHeader['cssFiles'] = array('css/css.css');
         $this->pageHeader['jsFiles'] = array('js/generic.js','js/recoverPassword.js');
         $this->pageHeader['bodyId'] = 'recover-password';
-        
+
+        $code = null;
         try
         {
             //Zjištění, zda je v adrese přítomen kód pro obnovu hesla
             if (!isset($parameters[0]))
             {
+                (new Logger(true))->notice('Vstup na stránku pro obnovení hesla byl uživateli na IP adrese {ip} zamítnut, protože URL adresa neobsahuje kód pro obnovu hesla', array('ip' => $_SERVER['REMOTE_ADDR']));
                 throw new AccessDeniedException(AccessDeniedException::REASON_RECOVER_NO_TOKEN, null, null);
             }
             $code = $parameters[0];
             $this->data['token'] = $code;
             
             $codeVerificator = new PasswordRecoveryCodeVerificator();
-            $userId = $codeVerificator::verifyCode($code);
+            $userId = $codeVerificator->verifyCode($code);
             if (empty($userId))
             {
+                (new Logger(true))->notice('Kód pro obnovu hesla (hash {hash}) odeslaný z IP adresy {ip} se nejeví jako platný', array('hash' => md5($code), 'ip' => $_SERVER['REMOTE_ADDR']));
                 throw new AccessDeniedException(AccessDeniedException::REASON_RECOVER_INVALID_TOKEN, null, null);
             }
-            
+            (new Logger(true))->info('Kód pro obnovu hesla (hash {hash}) odeslaný z IP adresy {ip} byl ověřen a uživateli byla umožněna změna hesla k uživatelskému účtu s ID {userId}', array('hash' => md5($code), 'ip' => $_SERVER['REMOTE_ADDR'], 'userId' => $userId));
+
             //Získat jméno uživatele pro zobrazení na stránce
             $username = Db::fetchQuery('SELECT '.User::COLUMN_DICTIONARY['name'].' FROM '.User::TABLE_NAME.' WHERE '.User::COLUMN_DICTIONARY['id'].' = ?', array($userId), false)[User::COLUMN_DICTIONARY['name']];
             
@@ -54,7 +61,12 @@ class RecoverPasswordController extends Controller
             $this->addMessage(MessageBox::MESSAGE_TYPE_ERROR, $e->getMessage());
             $this->redirect('');
         }
-        
+        catch (DatabaseException $e)
+        {
+            (new Logger(true))->alert('Uživatel přistupující do systému z IP adresy {ip} odeslal kód pro obnovu hesla (hash {hash}), který se však nepodařilo ověřit kvůli chybě databáze; je možné, že se k databázi nelze připojit', array('ip' => $_SERVER['REMOTE_ADDR'], 'hash' => md5($code)));
+            $this->addMessage(MessageBox::MESSAGE_TYPE_ERROR, AccessDeniedException::REASON_UNEXPECTED);
+        }
+
         if (isset($this->data['username']))
         {
             //Kód nalezen a uživatel identifikován
