ec0bbbbca8edff15b1c7ec5cd7bf9f6cde1d7eb6
Jan Štěch <honza.stech@gmail.com>
2021-02-25 11:54:13 +0100

    Doplnění @throws anotací do phpDocu v PasswordRecoverCodeVerificator.php
    Kód v souboru byl vyčištěn.

--- Models/PasswordRecoveryCodeVerificator.php
+++ Models/PasswordRecoveryCodeVerificator.php
@@ -1,6 +1,7 @@
 <?php
 namespace Poznavacky\Models;
 
+use Poznavacky\Models\Exceptions\DatabaseException;
 use Poznavacky\Models\Statics\Db;
 
 /** 
@@ -13,20 +14,19 @@ class PasswordRecoveryCodeVerificator
      * Metoda získávající ID uživatele s jehož účtem je svázán kód pro obnovu hesla uložený v databázi
      * @param string $code Kód pro obnovu hesla z URL adresy
      * @return int|boolean ID uživatele, který může použít tento kód pro obnovu svého hesla nebo FALSE, pokud kód nebyl v databázi nalezen
+     * @throws DatabaseException Pokud se při práci s databází vyskytne chyba
      */
     public function verifyCode(string $code)
     {
         $result = Db::fetchQuery('SELECT uzivatele_id FROM obnoveni_hesel WHERE kod = ? AND expirace > ?', array(md5($code), time()), false);
-        if (!$result)
-        {
-            return false;
-        }
+        if (!$result) { return false; }
         return $result['uzivatele_id'];
     }
-    
+
     /**
      * Metoda odstraňující specifikovaný kód pro obnovu hesla z databáze
      * @param string $code Nezašifrovaný kód k odstranění
+     * @throws DatabaseException Pokud se při práci s databází vyskytne chyba
      */
     public function deleteCode(string $code): void
     {
