15a336d0d971be268436073f53c8775b774b81cc
Jan Štěch <honza.stech@gmail.com>
2021-02-26 22:54:23 +0100

    Přidání logování a vyčištění kódu v EnterClassCodeController
    Nepotřebné části kódu jsou odstraněny

--- Controllers/Menu/EnterClassCodeController.php
+++ Controllers/Menu/EnterClassCodeController.php
@@ -7,6 +7,7 @@ use Poznavacky\Models\Security\AccessChecker;
 use Poznavacky\Models\Security\DataValidator;
 use Poznavacky\Models\Statics\ClassManager;
 use Poznavacky\Models\Statics\UserManager;
+use Poznavacky\Models\Logger;
 use Poznavacky\Models\MessageBox;
 
 /**
@@ -25,23 +26,27 @@ class EnterClassCodeController extends Controller
         $aChecker = new AccessChecker();
         if ($aChecker->checkDemoAccount())
         {
+            (new Logger(true))->warning('Uživatel demo účtu se pokusil zadat kód pro vstup do jiné než demo třídy z IP adresy {ip}', array('ip' => $_SERVER['REMOTE_ADDR']));
             $this->redirect('error403');
         }
 
-        if (!isset($_POST) || !isset($_POST['code']))
+        $userId = UserManager::getId();
+
+        if (!isset($_POST) || mb_strlen($_POST['code']) === 0)
         {
             //Chybně vyplněný formulář
+            (new Logger(true))->notice('Uživatel s ID {userId} odeslal formulář pro zadání vstupního kódu třídy z IP adresy {ip}, avšak nevyplnil do něj údaje', array('userId' => $userId, 'ip' => $_SERVER['REMOTE_ADDR']));
             $this->addMessage(MessageBox::MESSAGE_TYPE_ERROR, 'Musíte vyplnit kód třídy');
             $this->redirect('menu');
         }
         
         $code = $_POST['code'];
-        $userId = UserManager::getId();
         
         //Validace kódu
         $validator = new DataValidator();
         if (!$validator->validateClassCode($code))
         {
+            (new Logger(true))->notice('Uživatel s ID {userId} odeslal formulář pro zadání vstupního kódu třídy z IP adresy {ip}, avšak zadaný kód ({code}) nebyl ve správném formátu', array('userId' => $userId, 'ip' => $_SERVER['REMOTE_ADDR'], 'code' => $code));
             $this->addMessage(MessageBox::MESSAGE_TYPE_ERROR, 'Vstupní kód třídy musí být tvořen čtyřmi číslicemi');
             $this->redirect('menu');
         }
@@ -50,28 +55,22 @@ class EnterClassCodeController extends Controller
         if (count($classes) === 0)
         {
             //Se zadaným kódem se nelze dostat do žádné třídy
+            (new Logger(true))->notice('Uživatel s ID {userId} odeslal vstupní kód třídy z IP adresy {ip}, avšak zadaný kód ({$code}) není možné použít k získání přístupu do žádné třídy', array('userId' => $userId, 'ip' => $_SERVER['REMOTE_ADDR'], 'code' => $code));
             $this->addMessage(MessageBox::MESSAGE_TYPE_ERROR, 'Zadaný kód není platný');
             $this->redirect('menu');
         }
         
         $accessedClasses = array();
+        $accessedClassesIds = array();
         foreach($classes as $class)
         {
-            if ($class->getStatus() !== ClassObject::CLASS_STATUS_LOCKED && $class->addMember($userId))
-            {
                 $accessedClasses[] = $class->getName();
-            }
+                $accessedClassesIds[] = $class->getId();
         }
         
-        if (count($accessedClasses) > 0)
-        {
-            //Vypsat do zprávy pro uživatele jména tříd do kterých získal přístup uložená v $accessedClasses
-            $this->addMessage(MessageBox::MESSAGE_TYPE_SUCCESS, 'Získali jste přístup do následujících tříd: '.implode(', ',$accessedClasses));
-        }
-        else
-        {
-            $this->addMessage(MessageBox::MESSAGE_TYPE_ERROR, 'Žádné třídy s tímto přístupovým kódem nebyly nalezeny');
-        }
+		//Vypsat do zprávy pro uživatele jména tříd do kterých získal přístup uložená v $accessedClasses
+		(new Logger(true))->info('Uživatel s ID {userId} odeslal vstupní kód třídy ({code}) z IP adresy {ip} a získal přístup do tříd/y s ID {classesIds}', array('userId' => $userId, 'code' => $code, 'ip' => $_SERVER['REMOTE_ADDR'], 'classesIds' => implode(', ', $accessedClassesIds)));
+		$this->addMessage(MessageBox::MESSAGE_TYPE_SUCCESS, 'Získali jste přístup do následujících tříd: '.implode(', ',$accessedClasses));
         
         $this->redirect('menu');
     }
