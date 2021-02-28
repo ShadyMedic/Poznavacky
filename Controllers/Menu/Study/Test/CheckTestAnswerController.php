ffd48322eb6cf73cb74af77d35368e5e5bb98da8
Jan Štěch <honza.stech@gmail.com>
2021-02-28 20:04:54 +0100

    Přidání logování a vyčištění kódu v CheckTestAnswerController.php

--- Controllers/Menu/Study/Test/CheckTestAnswerController.php
+++ Controllers/Menu/Study/Test/CheckTestAnswerController.php
@@ -6,6 +6,7 @@ use Poznavacky\Models\Exceptions\AccessDeniedException;
 use Poznavacky\Models\Statics\UserManager;
 use Poznavacky\Models\AjaxResponse;
 use Poznavacky\Models\AnswerChecker;
+use Poznavacky\Models\Logger;
 
 /** 
  * Kontroler volaný pomocí AJAX, který ověřuje odpověď zadanou uživatelem na testovací stránce
@@ -15,6 +16,7 @@ class CheckTestAnswerController extends Controller
 {
     /**
      * Metoda načítající odpověď z $_POST a ověřuje jí proti správné odpovědi uložené v $_SESSION
+     * @param array $parameters Parametry pro zpracování kontrolerem (nevyužíváno)
      * @see Controller::process()
      */
     public function process(array $parameters): void
@@ -31,6 +33,7 @@ class CheckTestAnswerController extends Controller
         catch(AccessDeniedException $e)
         {
             //Neplatné číslo otázky nebo jiná chyba při ověřování odpovědi
+            (new Logger(true))->notice('Uživatel s ID {userId} přistupující do systému z IP adresy {ip} odeslal svou odpověď na obrázek číslo {questionNum} na zkoušecí stránce části/í poznávačky s ID {groupId}, avšak správná odpověď nebyla v úložišti sezení nalezena', array('userId' => UserManager::getId(), 'ip' => $_SERVER['REMOTE_ADDR'], 'questionNum' => $questionNum, 'groupId' => $_SESSION['selection']['group']->getId()));
             $response = new AjaxResponse(AjaxResponse::MESSAGE_TYPE_ERROR, $e->getMessage());
             echo $response->getResponseString();
             exit();
@@ -38,12 +41,14 @@ class CheckTestAnswerController extends Controller
         
         if ($result)
         {
+            (new Logger(true))->info('Odpověď uživatele s ID {userId} přistupujícího do systému z IP adresy {ip} na otázku číslo {questionNum} na zkoušecí stránce části/í poznávačky s ID {groupId} byla vyhodnocena jako správná', array('userId' => UserManager::getId(), 'ip' => $_SERVER['REMOTE_ADDR'], 'questionNum' => $questionNum, 'groupId' => $_SESSION['selection']['group']->getId()));
             UserManager::getUser()->incrementGuessedPictures();
             $response = new AjaxResponse(AjaxResponse::MESSAGE_TYPE_INFO, 'correct', array('answer' => $checker->lastSavedAnswer));
             echo $response->getResponseString();
         }
         else
         {
+            (new Logger(true))->info('Odpověď uživatele s ID {userId} přistupujícího do systému z IP adresy {ip} na otázku číslo {questionNum} na zkoušecí stránce části/í poznávačky s ID {groupId} byla vyhodnocena jako nesprávná', array('userId' => UserManager::getId(), 'ip' => $_SERVER['REMOTE_ADDR'], 'questionNum' => $questionNum, 'groupId' => $_SESSION['selection']['group']->getId()));
             $response = new AjaxResponse(AjaxResponse::MESSAGE_TYPE_INFO, 'wrong', array('answer' => $checker->lastSavedAnswer));
             echo $response->getResponseString();
         }
