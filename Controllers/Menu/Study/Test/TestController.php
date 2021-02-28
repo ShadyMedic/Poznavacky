73d375499473c99e0b01b1dcabcc1a34021c30ec
Jan Štěch <honza.stech@gmail.com>
2021-02-28 19:48:43 +0100

    Přidání logování a vyčištění kódu v kontrolerech pro testovací stránku
    To se netýká kontroleru pro kontrolu odpovědi, pouze kontrolerů:
    TestController
    TestPicturesController

--- Controllers/Menu/Study/Test/TestController.php
+++ Controllers/Menu/Study/Test/TestController.php
@@ -2,8 +2,9 @@
 namespace Poznavacky\Controllers\Menu\Study\Test;
 
 use Poznavacky\Controllers\Controller;
-use Poznavacky\Models\MessageBox;
 use Poznavacky\Models\Statics\UserManager;
+use Poznavacky\Models\Logger;
+use Poznavacky\Models\MessageBox;
 
 /** 
  * Kontroler starající se o výpis stránky pro testování
@@ -13,13 +14,16 @@ class TestController extends Controller
 {
     /**
      * Metoda ověřující, zda má uživatel do třídy přístup a nastavující hlavičku stránky a pohled
+     * @param array $parameters Pole parametrů pro zpracování kontrolerem, zde může jako první prvek obsahovat URL název dalšího kontroleru, kterému má tento kontroler předat řízení
      * @see Controller::process()
      */
     public function process(array $parameters): void
     {
         //Kontrola přístupu již proběhla v MenuController.php
 
+        $class = $_SESSION['selection']['group'];
         $group = $_SESSION['selection']['group'];
+        $part = null;
         if (isset($_SESSION['selection']['part']))
         {
             $part = $_SESSION['selection']['part'];
@@ -37,6 +41,7 @@ class TestController extends Controller
         )
         {
             //Žádné přírodniny
+            (new Logger(true))->warning('Uživatel s ID {userId} se pokusil přistoupit na stránku pro zkoušení z části/í poznávačky s ID {groupId} patřící do třídy s ID {classId} z IP adresy {ip}, ačkoliv tato poznávačka/část neobsahuje žádné přírodniny', array('userId' => UserManager::getId(), 'groupId' => $group->getId(), 'classId' => $class->getId(), 'ip' => $_SERVER['REMOTE_ADDR']));
             $this->addMessage(MessageBox::MESSAGE_TYPE_ERROR, "V této části nebo poznávačce nejsou zatím přidané žádné přírodniny");
             $this->redirect('menu/'.$_SESSION['selection']['class']->getUrl().'/'.$_SESSION['selection']['group']->getUrl());
         }
@@ -48,16 +53,7 @@ class TestController extends Controller
         $this->pageHeader['jsFiles'] = array('js/generic.js','js/ajaxMediator.js','js/test.js','js/reportForm.js','js/menu.js');
         $this->pageHeader['bodyId'] = 'test';
 
-        if (!$allParts)
-        {
-            $this->data['navigationBar'] = array(
-                0 => array(
-                    'text' => $this->pageHeader['title'],
-                    'link' => 'menu/'.$_SESSION['selection']['class']->getUrl().'/'.$_SESSION['selection']['group']->getUrl().'/'.$_SESSION['selection']['part']->getUrl().'/test'
-                )
-            );
-        }
-        else
+        if ($allParts)
         {
             $this->data['navigationBar'] = array(
                 0 => array(
@@ -65,6 +61,19 @@ class TestController extends Controller
                     'link' => 'menu/'.$_SESSION['selection']['class']->getUrl().'/'.$_SESSION['selection']['group']->getUrl().'/test'
                 )
             );
+
+            (new Logger(true))->info('Přístup na stránku pro zkoušení ze všech částí poznávačky s ID {groupId} patřící do třídy s ID {classId} uživatelem s ID {userId} z IP adresy {ip}', array('groupId' => $group->getId(), 'classId' => $class->getId(), 'userId' => UserManager::getId(), 'ip' => $_SERVER['REMOTE_ADDR']));
+        }
+        else
+        {
+            $this->data['navigationBar'] = array(
+                0 => array(
+                    'text' => $this->pageHeader['title'],
+                    'link' => 'menu/'.$_SESSION['selection']['class']->getUrl().'/'.$_SESSION['selection']['group']->getUrl().'/'.$_SESSION['selection']['part']->getUrl().'/test'
+                )
+            );
+
+            (new Logger(true))->info('Přístup na stránku pro zkoušení z části s ID {partId} patřící do poznávačky s ID {groupId} patřící do třídy s ID {classId} uživatelem s ID {userId} z IP adresy {ip}', array('partId' => $part->getId(), 'groupId' => $group->getId(), 'classId' => $class->getId(), 'userId' => UserManager::getId(), 'ip' => $_SERVER['REMOTE_ADDR']));
         }
 
         $controllerName = "nonexistant-controller";
--- Controllers/Menu/Study/Test/TestPicturesController.php
+++ Controllers/Menu/Study/Test/TestPicturesController.php
@@ -2,9 +2,10 @@
 namespace Poznavacky\Controllers\Menu\Study\Test;
 
 use Poznavacky\Controllers\Controller;
+use Poznavacky\Models\Exceptions\DatabaseException;
+use Poznavacky\Models\Logger;
 use Poznavacky\Models\Statics\UserManager;
 use Poznavacky\Models\AjaxResponse;
-use Poznavacky\Models\MessageBox;
 
 /** 
  * Kontroler volaný pomocí AJAX, který zajišťuje odeslání adresy obrázků pro testovací stránku
@@ -13,17 +14,28 @@ use Poznavacky\Models\MessageBox;
 class TestPicturesController extends Controller
 {
     private const PICTURES_SENT_PER_REQUEST = 20;
-    
+
     /**
      * Metoda odesílající daný počet náhodně zvolených obrázků ze zvolené části/přírodniny
      * Adresy jsou odeslány jako pole v JSON formátu
+     * @param array $parameters Parametry pro zpracování kontrolerem (nevyužíváno)
      * @see Controller::process()
      */
     public function process(array $parameters): void
     {
         //Kontrola přístupu již proběhla v MenuController.php
 
+        //Kontrola, zda byl tento kontroler zavolán jako AJAX
+        if (empty($_SERVER['HTTP_X_REQUESTED_WITH']) || strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest' )
+        {
+            (new Logger(true))->warning('Uživatel s ID {userId} se pokusil přistoupit ke kontroleru test-pictures z IP adresy {ip} jinak než pomocí AJAX požadavku', array('userId' => UserManager::getId(), 'ip' => $_SERVER['REMOTE_ADDR']));
+            header('HTTP/1.0 400 Bad Request');
+            exit();
+        }
+
+        $class = $_SESSION['selection']['class'];
         $group = $_SESSION['selection']['group'];
+        $part = null;
         if (isset($_SESSION['selection']['part']))
         {
             $part = $_SESSION['selection']['part'];
@@ -41,21 +53,30 @@ class TestPicturesController extends Controller
         )
         {
             //Žádné přírodniny
+            (new Logger(true))->warning('Uživatel s ID {userId} se pokusil získat náhodné obrázky pro zkoušecí stránku poznávačky s ID {groupId} z IP adresy {ip}, avšak zvolená poznávačka/část neobsahuje žádné přírodniny', array('userId' => UserManager::getId(), 'groupId' => $group->getId(), 'ip' => $_SERVER['REMOTE_ADDR']));
             header('HTTP/1.0 400 Bad Request');
             exit();
         }
 
         //Získání objektů obrázků
-        if (isset($_SESSION['selection']['part']))
+        $pictures = array();
+        try
         {
-            $part = $_SESSION['selection']['part'];
-            $pictures = $part->getRandomPictures(self::PICTURES_SENT_PER_REQUEST);
+            if (isset($_SESSION['selection']['part']))
+            {
+                $part = $_SESSION['selection']['part'];
+                $pictures = $part->getRandomPictures(self::PICTURES_SENT_PER_REQUEST);
+            }
+            else
+            {
+                $pictures = $group->getRandomPictures(self::PICTURES_SENT_PER_REQUEST);
+            }
         }
-        else
+        catch (DatabaseException $e)
         {
-            $pictures = $group->getRandomPictures(self::PICTURES_SENT_PER_REQUEST);
+            (new Logger(true))->alert('Uživatel s ID {userId} zažádal o náhodné obrázky pro zkoušecí stránku poznávačky s ID {groupId} z IP adresy {ip}, avšak při jejich načítání došlo k chybě databáze; pokud toto není ojedinělá chyba, je možné, že tato část systému nefunguje nikomu; chybová hláška: {exception}', array('userId' => UserManager::getId(), 'groupId' => $group->getId(), 'ip' => $_SERVER['REMOTE_ADDR'], 'exception' => $e));
         }
-        
+
         //Vymazání předchozích odpovědí
         unset($_SESSION['testAnswers']);
         
@@ -68,6 +89,7 @@ class TestPicturesController extends Controller
         }
         
         //Odeslání dvourozměrného pole s čísly otázek a adresami obrázků
+        (new Logger(true))->info('K uživateli s ID {userId} přistupujícímu do systému z IP adresy {ip} byly odeslány náhodné obrázky pro zkoušecí stránku části/částí poznávačky s ID {groupId} patřící do třídy s ID {classId}', array('userId' => UserManager::getId(), 'ip' => $_SERVER['REMOTE_ADDR'], 'groupId' => $group->getId(), 'classId' => $class->getId()));
         header('Content-Type: application/json');
         $response = new AjaxResponse(AjaxResponse::MESSAGE_TYPE_SUCCESS, '', array('pictures' => $picturesArr));
         echo $response->getResponseString();
