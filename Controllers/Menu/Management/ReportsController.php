6eea867d472db2d45370acb4225b61bec9760882
Jan Štěch <honza.stech@gmail.com>
2021-04-04 22:49:04 +0200

    Přidání logování při přístupu na stránku pro správu hlášení

--- Controllers/Menu/Management/ReportsController.php
+++ Controllers/Menu/Management/ReportsController.php
@@ -2,6 +2,8 @@
 namespace Poznavacky\Controllers\Menu\Management;
 
 use Poznavacky\Controllers\SynchronousController;
+use Poznavacky\Models\Logger;
+use Poznavacky\Models\Statics\UserManager;
 
 /**
  * Kontroler starající se o stránku se správou hlášení pro administrátory tříd
@@ -17,6 +19,8 @@ class ReportsController extends SynchronousController
      */
     public function process(array $parameters): void
     {
+        (new Logger(true))->info('Přístup na stránku pro správu hlášení v poznávačce s ID {groupId} patřící do třídy s ID {classId} uživatelem s ID {userId} z IP adresy {ip}', array('groupId' => $_SESSION['selection']['group']->getId(), 'classId' => $_SESSION['selection']['class']->getId(), 'userId' => UserManager::getId(), 'ip' => $_SERVER['REMOTE_ADDR']));
+
         $group = $_SESSION['selection']['group'];
         self::$data['reports'] = $group->getReports();
         self::$data['naturalsInGroup'] = $group->getNaturals();
