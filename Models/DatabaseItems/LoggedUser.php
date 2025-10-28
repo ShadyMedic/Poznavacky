93f551ead36c957c6e151013c6d83d99c64f2d38
Jan Štěch <jan.stech@posteo.net>
2025-10-28 18:33:04 +0100

    Oprava napojení výběru motivu na databázi a jeho aplikace na <body>

--- Models/DatabaseItems/LoggedUser.php
+++ Models/DatabaseItems/LoggedUser.php
@@ -6,6 +6,7 @@ use Poznavacky\Models\Exceptions\DatabaseException;
 use Poznavacky\Models\Security\AccessChecker;
 use Poznavacky\Models\Security\DataValidator;
 use Poznavacky\Models\Statics\Db;
+use Poznavacky\Models\Statics\Settings;
 use Poznavacky\Models\Statics\UserManager;
 use Poznavacky\Models\Logger;
 use Poznavacky\Models\undefined;
@@ -27,7 +28,7 @@ class LoggedUser extends User
         'lastLogin' => 'posledni_prihlaseni',
         'lastChangelog' => 'posledni_changelog',
         'lastMenuTableUrl' => 'adresa_posledni_slozky',
-        'darkTheme' => 'tmavy_motiv',
+        'theme' => 'motiv',
         'addedPictures' => 'pridane_obrazky',
         'guessedPictures' => 'uhodnute_obrazky',
         'karma' => 'karma',
@@ -41,7 +42,7 @@ class LoggedUser extends User
         'email' => null,
         'lastChangelog' => 0,
         'lastMenuTableUrl' => null,
-        'darkTheme' => 0,
+        'theme' => null,
         'addedPictures' => 0,
         'guessedPictures' => 0,
         'karma' => 0,
@@ -54,7 +55,7 @@ class LoggedUser extends User
     protected $hash;
     protected $lastChangelog;
     protected $lastMenuTableUrl;
-    protected $darkTheme;
+    protected $theme;
     
     /**
      * Metoda nastavující všechny vlasnosti objektu (s výjimkou ID) podle zadaných argumentů
@@ -70,13 +71,13 @@ class LoggedUser extends User
      * @param string|undefined|null $hash Heš uživatelova hesla z databáze
      * @param float|undefined|null $lastChangelog Poslední zobrazený changelog
      * @param int|undefined|null $lastMenuTableUrl URL poslední navštívené složky na menu stránce
-     * @param bool|undefined|null $darkTheme TRUE, pokud je povolen tmavý mód, FALSE, pokud ne
+     * @param string|undefined|null $theme 'dark', pokud je povolen tmavý mód, 'light', pokud ne; UNDEFINED pokud zatím nebylo zvoleno
      * {@inheritDoc}
      * @see User::initialize()
      */
     public function initialize($name = null, $email = null, $lastLogin = null, $addedPictures = null,
                                $guessedPictures = null, $karma = null, $status = null, $hash = null,
-                               $lastChangelog = null, $lastMenuTableUrl = null, $darkTheme = null): void
+                               $lastChangelog = null, $lastMenuTableUrl = null, $theme = null): void
     {
         //Nastav vlastnosti zděděné z mateřské třídy
         parent::initialize($name, $email, $lastLogin, $addedPictures, $guessedPictures, $karma, $status);
@@ -91,14 +92,18 @@ class LoggedUser extends User
         if ($lastMenuTableUrl === null) {
             $lastMenuTableUrl = $this->lastMenuTableUrl;
         }
-        if ($darkTheme === null) {
-            $darkTheme = $this->darkTheme;
+        if ($theme === null) {
+            $theme = $this->theme;
         }
-        
+
+        if ($theme instanceof undefined) {
+            $theme = null;
+        }
+
         $this->hash = $hash;
         $this->lastChangelog = $lastChangelog;
         $this->lastMenuTableUrl = $lastMenuTableUrl;
-        $this->darkTheme = $darkTheme;
+        $this->theme = $theme;
     }
     
     /**
@@ -387,9 +392,9 @@ class LoggedUser extends User
     {
         $this->loadIfNotLoaded($this->id);
 
-        $this->darkTheme = $darkThemeSelected;
-        return Db::executeQuery('UPDATE '.self::TABLE_NAME.' SET '.self::COLUMN_DICTIONARY['darkTheme'].
-            ' = ? WHERE '.self::COLUMN_DICTIONARY['id'].' = ?', array((int)$darkThemeSelected, $this->id));
+        $this->theme = $darkThemeSelected ? 'dark' : 'light';
+        return Db::executeQuery('UPDATE '.self::TABLE_NAME.' SET '.self::COLUMN_DICTIONARY['theme'].
+            ' = ? WHERE '.self::COLUMN_DICTIONARY['id'].' = ?', array($this->theme, $this->id));
     }
     
     /**
