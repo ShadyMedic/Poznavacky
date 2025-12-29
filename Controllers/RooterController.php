aff7982b62647ddec37880889f6845b25497cdb9
Jan Štěch <jan.stech@posteo.net>
2025-12-29 10:44:42 +0100

    Oprava některých deprecated varování

--- Controllers/RooterController.php
+++ Controllers/RooterController.php
@@ -100,7 +100,7 @@ class RooterController extends SynchronousController
         }
         
         //Získání seznamu nastavní složek
-        $selections = explode(self::INI_ARRAY_SEPARATOR, @$iniRoutes['Selections'][$path]);
+        $selections = explode(self::INI_ARRAY_SEPARATOR, $iniRoutes['Selections'][$path] ?? '');
         if (empty($selections[0])) {
             $selections = array();
         }
@@ -116,13 +116,13 @@ class RooterController extends SynchronousController
             $shortPath = $path;
         }
         
-        $checks = explode(self::INI_ARRAY_SEPARATOR, @$iniRoutes['Checks'][$shortPath]);
+        $checks = explode(self::INI_ARRAY_SEPARATOR, $iniRoutes['Checks'][$shortPath] ?? '');
         if (empty($checks[0])) {
             $checks = array();
         }
         
         //Získání seznamu pohledu pro použití
-        self::$views = explode(self::INI_ARRAY_SEPARATOR, @$iniRoutes['Views'][$shortPath]);
+        self::$views = explode(self::INI_ARRAY_SEPARATOR, $iniRoutes['Views'][$shortPath] ?? '');
         if (empty(self::$views[0])) {
             self::$views = array();
         }
