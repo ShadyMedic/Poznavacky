6044e08af6e49140b3bfda4fdcfedc2270b1c155
eksyska <eli.korcova@gmail.com>
2026-07-19 21:13:59 +0200

    Připraven pohled pro nový režim
    Zatím je všude použito označení test-new a jeho varianty v různém case. Nejspíš bude dobré přejmenovat původní test->procvičování a tento nový režim se pak bude jmenovat test, je třeba vyřešit v budoucnu.
    
    v css souboru jsou nějaké drobné změny navíc kvůli aktualizaci Sass a změn v kompilaci

---
+++ Controllers/Menu/Study/TestNew/TestNewController.php
@@ -0,0 +1,61 @@
+<?php
+namespace Poznavacky\Controllers\Menu\Study\Testnew;
+
+use Poznavacky\Controllers\SynchronousController;
+use Poznavacky\Models\Exceptions\AccessDeniedException;
+use Poznavacky\Models\Security\AccessChecker;
+use Poznavacky\Models\Statics\UserManager;
+use Poznavacky\Models\Logger;
+
+
+/* TODO pouze provizorní Controller vzniklý kopií původního TestControlleru pro nový režim */
+
+/**
+ * Kontroler starající se o výpis stránky pro testování
+ * @author Jan Štěch
+ */
+class TestNewController extends SynchronousController
+{
+    /**
+     * Metoda nastavující hlavičku stránky a pohled
+     * @param array $parameters Parametry pro zpracování kontrolerem (nevyužíváno)
+     * @throws AccessDeniedException Pokud není přihlášen žádný uživatel
+     * @see SynchronousController::process()
+     */
+    public function process(array $parameters): void
+    {
+        self::$pageHeader['title'] = 'Vyzkoušet se';
+        self::$pageHeader['description'] = 'Vyzkoušejte si, jak dobře znáte přírodniny v poznávačce pomocí náhodného testování';
+        self::$pageHeader['keywords'] = '';
+        self::$pageHeader['cssFiles'] = array('css/menu.css');
+        self::$pageHeader['jsFiles'] = array(
+            'js/generic.js',
+            'js/ajaxMediator.js',
+            'js/testNew.js',
+            'js/reportForm.js',
+            'js/menu.js'
+        );
+        self::$pageHeader['bodyId'] = 'test-new';
+        
+        $aChecker = new AccessChecker();
+        if (!$aChecker->checkPart()) {
+            (new Logger())->info('Přístup na stránku pro zkoušení ze všech částí poznávačky s ID {groupId} patřící do třídy s ID {classId} uživatelem s ID {userId} z IP adresy {ip}',
+                array(
+                    'groupId' => $_SESSION['selection']['group']->getId(),
+                    'classId' => $_SESSION['selection']['class']->getId(),
+                    'userId' => UserManager::getId(),
+                    'ip' => $_SERVER['REMOTE_ADDR']
+                ));
+        } else {
+            (new Logger())->info('Přístup na stránku pro zkoušení z části s ID {partId} patřící do poznávačky s ID {groupId} patřící do třídy s ID {classId} uživatelem s ID {userId} z IP adresy {ip}',
+                array(
+                    'partId' => $_SESSION['selection']['part']->getId(),
+                    'groupId' => $_SESSION['selection']['group']->getId(),
+                    'classId' => $_SESSION['selection']['class']->getId(),
+                    'userId' => UserManager::getId(),
+                    'ip' => $_SERVER['REMOTE_ADDR']
+                ));
+        }
+    }
+}
+
